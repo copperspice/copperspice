6cbf07a289f1d203ee1b73edc226e5b5e8e73bb2
Giuseppe D'Angelo <dangelog@gmail.com>
2012-04-01 18:47:56 +0100

    QFileSystemModel: fix sorting
    When sorting a model recursively, the children of a QFileSystemNode
    are extracted from their parent in a QHash order; then filtered,
    then sorted (using a stable sort) depending on the sorting column.
    
    This means that the order of the children comparing to equal for
    the chosen sort are shown in the order they were picked from the
    iteration on the QHash, which isn't reliable at all.
    
    Moreover, the criteria used in QFileSystemModelSorter for sorting
    are too loose: when sorting by any column but the name, if the result
    is "equality", then the file names should be used to determine
    the sort order.
    
    This patch removes the stable sort in favour of a full sort,
    and fixes the criteria of soring inside QFileSystemModelSorter.
    
    Change-Id: Idd9aece22f2ebbe77ec40d372b43cde4c200ff38
    Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>
    (cherry picked from commit e6e4456de0506aa9896b687dc858eb9ae03d8917)

--- src/gui/dialogs/qfilesystemmodel.cpp
+++ src/gui/dialogs/qfilesystemmodel.cpp
@@ -32,6 +32,8 @@
 #include <qmessagebox.h>
 #include <qapplication.h>
 
+#include <algorithm>
+
 #ifdef Q_OS_WIN
 #include <qt_windows.h>
 #endif
@@ -999,17 +1001,36 @@ class QFileSystemModelSorter
             return QFileSystemModelPrivate::naturalCompare(l->fileName,
                    r->fileName, Qt::CaseInsensitive) < 0;
          }
-         case 1:
-            // Directories go first
-            if (l->isDir() && !r->isDir()) {
-               return true;
-            }
-            return l->size() < r->size();
+      case 1: {
+            // Directories go first	    
+	    bool left = l->isDir();
+	    bool right = r->isDir();
+	    if (left ^ right) {
+	       return left;
+	    }
+	    
+	    qint64 sizeDifference = l->size() - r->size();
+	    if (sizeDifference == 0) {
+	       return QFileSystemModelPrivate::naturalCompare(l->fileName, r->fileName, Qt::CaseInsensitive) < 0;
+	    }
+	    
+	    return sizeDifference < 0;
+      }
 
-         case 2:
-            return l->type() < r->type();
+         case 2: {
+	    int compare = QString::localeAwareCompare(l->type(), r->type());
+	    if (compare == 0) {
+	       return QFileSystemModelPrivate::naturalCompare(l->fileName, r->fileName, Qt::CaseInsensitive) < 0;
+	    }
+	    
+	    return compare < 0;
+         }
 
          case 3:
+	    if (l->lastModified() == r->lastModified()) {
+	       return QFileSystemModelPrivate::naturalCompare(l->fileName, r->fileName, Qt::CaseInsensitive) < 0;
+	    }
+
             return l->lastModified() < r->lastModified();
       }
 
@@ -1053,7 +1074,7 @@ void QFileSystemModelPrivate::sortChildren(int column, const QModelIndex &parent
    }
 
    QFileSystemModelSorter ms(column);
-   qStableSort(values.begin(), values.end(), ms);
+   std::sort(values.begin(), values.end(), ms);
 
    // update the new visible list
    indexNode->visibleChildren.clear();
