51df38b6a905f32604ba8e3f35c1819f4f982efd
David Garcia <david@aimsun.com>
2014-02-05 18:49:11 +0100

    Mac: Fix mixing unified and non-unified toolbars
    Test if the target toolbar is actually a unified
    toolbar before calling the special MapFromParent
    function.
    
    This change is not needed in Qt 5.
    
    Task-number: QTBUG-36100
    Change-Id: I37fce89c79f5fce057e0b31d4c4f2e72269cdb06
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@digia.com>
    (cherry picked from commit e66dd978999358efdb4d3edcc89b0455194c4ae1)

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -6935,8 +6935,8 @@ QWidget *QWidgetPrivate::childAtRecursiveHelper(const QPoint &p, bool ignoreChil
       // 'includeFrame' is true if the child's parent is a top-level QMainWindow with an unified tool bar.
       // An unified tool bar on the Mac lives outside QMainWindow's geometry(), so a normal
       // QWidget::mapFromParent won't do the trick.
-      if (includeFrame && qobject_cast<QToolBar *>(child)) {
-         childPoint = qt_mac_nativeMapFromParent(child, p);
+      if (includeFrame && qobject_cast<QToolBar *>(child) && qt_widget_private(child)->isInUnifiedToolbar) {
+	 childPoint = qt_mac_nativeMapFromParent(child, p);
       } else
 #endif
          childPoint -= child->data->crect.topLeft();
