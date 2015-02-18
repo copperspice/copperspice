5d8dc2328c1918356b0a08bdc574f079608dcfb5
Matteo Bertini <matteo@naufraghi.net>
2015-02-18 17:10:41 +0100

    Identify XButton1|2 release events when mouse is moving.
    X11 has no special state for XButton1 and XButton2, so we need
    to look at the global `mouseButtonState` and restore the buttons
    after a mouse move.
    
    Task-number: QTBUG-40148
    Change-Id: I789f32b344cf7902b1c7aa6b7da7f20f32602be2
    Reviewed-by: Alexander Volkov <a.volkov@rusbitech.ru>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@digia.com>
    (cherry picked from commit 363c9aba772b20d2df9070be730ffb36775b6f2b)

--- src/gui/kernel/qapplication_x11.cpp
+++ src/gui/kernel/qapplication_x11.cpp
@@ -4335,6 +4335,15 @@ static Qt::MouseButtons translateMouseButtons(int s)
    if (s & Button3Mask) {
       ret |= Qt::RightButton;
    }
+   // X11 has no special state for XButton1 and XButton2, so we need to use
+   // the global state maintained between press and release.
+   if (mouseButtonState.testFlag(Qt::XButton1)) {
+      ret |= Qt::XButton1;
+   }
+   if (mouseButtonState.testFlag(Qt::XButton2)) {
+      ret |= Qt::XButton2;
+   }
+
    return ret;
 }
 
