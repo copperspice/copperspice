e4187155527e8e0b084ac63d1d14025a895aedeb
Ansel Sermersheim <ansel@copperspice.com>
2015-05-04 23:56:55 -0700

    Removed BSD 4 clause licensed code in qlocale_tools.cpp

--- src/core/tools/qlocale_tools.cpp
+++ src/core/tools/qlocale_tools.cpp
@@ -33,6 +33,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <cstdlib>
 
 #if defined(Q_OS_LINUX) && !defined(__UCLIBC__)
 #    include <fenv.h>
@@ -251,43 +252,6 @@ bool removeGroupSeparators(QLocalePrivate::CharBuff *num)
    return true;
 }
 
-
-/*-
- * Copyright (c) 1992, 1993
- *        The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgment:
- *        This product includes software developed by the University of
- *        California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-// static char sccsid[] = "@(#)strtouq.c        8.1 (Berkeley) 6/4/93";
-//  "$FreeBSD: src/lib/libc/stdlib/strtoull.c,v 1.5.2.1 2001/03/02 09:45:20 obrien Exp $";
-
 /*
  * Convert a string to an unsigned long long integer.
  *
@@ -296,88 +260,21 @@ bool removeGroupSeparators(QLocalePrivate::CharBuff *num)
  */
 qulonglong qstrtoull(const char *nptr, const char **endptr, int base, bool *ok)
 {
-   const char *s = nptr;
-   qulonglong acc;
-   unsigned char c;
-   qulonglong qbase, cutoff;
-   int any, cutlim;
+    char * end = const_cast<char *>(nptr);
+    qulonglong retval = std::strtoull(nptr, &end, base);
 
-   if (ok != 0) {
-      *ok = true;
-   }
+    if(end != nptr) {
+	*ok = true;
+    } else {
+	*ok = false;
+    }
 
-   /*
-    * See strtoq for comments as to the logic used.
-    */
-   s = nptr;
-   do {
-      c = *s++;
-   } while (isspace(c));
-   if (c == '-') {
-      if (ok != 0) {
-         *ok = false;
-      }
-      if (endptr != 0) {
-         *endptr = s - 1;
-      }
-      return 0;
-   } else {
-      if (c == '+') {
-         c = *s++;
-      }
-   }
-   if ((base == 0 || base == 16) &&
-         c == '0' && (*s == 'x' || *s == 'X')) {
-      c = s[1];
-      s += 2;
-      base = 16;
-   }
-   if (base == 0) {
-      base = c == '0' ? 8 : 10;
-   }
-   qbase = unsigned(base);
-   cutoff = qulonglong(ULLONG_MAX) / qbase;
-   cutlim = qulonglong(ULLONG_MAX) % qbase;
-   for (acc = 0, any = 0;; c = *s++) {
-      if (!isascii(c)) {
-         break;
-      }
-      if (isdigit(c)) {
-         c -= '0';
-      } else if (isalpha(c)) {
-         c -= isupper(c) ? 'A' - 10 : 'a' - 10;
-      } else {
-         break;
-      }
-      if (c >= base) {
-         break;
-      }
-      if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim)) {
-         any = -1;
-      } else {
-         any = 1;
-         acc *= qbase;
-         acc += c;
-      }
-   }
-   if (any == 0) {
-      if (ok != 0) {
-         *ok = false;
-      }
-   } else if (any < 0) {
-      acc = ULLONG_MAX;
-      if (ok != 0) {
-         *ok = false;
-      }
-   }
-   if (endptr != 0) {
-      *endptr = (any ? s - 1 : nptr);
-   }
-   return acc;
+    if(endptr) {
+	*endptr = end;
+    }
+
+    return retval;
 }
-
-
-//  "$FreeBSD: src/lib/libc/stdlib/strtoll.c,v 1.5.2.1 2001/03/02 09:45:20 obrien Exp $";
 
 
 /*
@@ -388,101 +285,20 @@ qulonglong qstrtoull(const char *nptr, const char **endptr, int base, bool *ok)
  */
 qlonglong qstrtoll(const char *nptr, const char **endptr, int base, bool *ok)
 {
-   const char *s;
-   qulonglong acc;
-   unsigned char c;
-   qulonglong qbase, cutoff;
-   int neg, any, cutlim;
+    char * end = const_cast<char *>(nptr);
+    qlonglong retval = std::strtoll(nptr, &end, base);
 
-   /*
-    * Skip white space and pick up leading +/- sign if any.
-    * If base is 0, allow 0x for hex and 0 for octal, else
-    * assume decimal; if base is already 16, allow 0x.
-    */
-   s = nptr;
-   do {
-      c = *s++;
-   } while (isspace(c));
-   if (c == '-') {
-      neg = 1;
-      c = *s++;
-   } else {
-      neg = 0;
-      if (c == '+') {
-         c = *s++;
-      }
-   }
-   if ((base == 0 || base == 16) &&
-         c == '0' && (*s == 'x' || *s == 'X')) {
-      c = s[1];
-      s += 2;
-      base = 16;
-   }
-   if (base == 0) {
-      base = c == '0' ? 8 : 10;
-   }
+    if(end != nptr) {
+	*ok = true;
+    } else {
+	*ok = false;
+    }
 
-   /*
-    * Compute the cutoff value between legal numbers and illegal
-    * numbers.  That is the largest legal value, divided by the
-    * base.  An input number that is greater than this value, if
-    * followed by a legal input character, is too big.  One that
-    * is equal to this value may be valid or not; the limit
-    * between valid and invalid numbers is then based on the last
-    * digit.  For instance, if the range for quads is
-    * [-9223372036854775808..9223372036854775807] and the input base
-    * is 10, cutoff will be set to 922337203685477580 and cutlim to
-    * either 7 (neg==0) or 8 (neg==1), meaning that if we have
-    * accumulated a value > 922337203685477580, or equal but the
-    * next digit is > 7 (or 8), the number is too big, and we will
-    * return a range error.
-    *
-    * Set any if any `digits' consumed; make it negative to indicate
-    * overflow.
-    */
-   qbase = unsigned(base);
-   cutoff = neg ? qulonglong(0 - (LLONG_MIN + LLONG_MAX)) + LLONG_MAX : LLONG_MAX;
-   cutlim = cutoff % qbase;
-   cutoff /= qbase;
-   for (acc = 0, any = 0;; c = *s++) {
-      if (!isascii(c)) {
-         break;
-      }
-      if (isdigit(c)) {
-         c -= '0';
-      } else if (isalpha(c)) {
-         c -= isupper(c) ? 'A' - 10 : 'a' - 10;
-      } else {
-         break;
-      }
-      if (c >= base) {
-         break;
-      }
-      if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim)) {
-         any = -1;
-      } else {
-         any = 1;
-         acc *= qbase;
-         acc += c;
-      }
-   }
-   if (any < 0) {
-      acc = neg ? LLONG_MIN : LLONG_MAX;
-      if (ok != 0) {
-         *ok = false;
-      }
-   } else if (neg) {
-      acc = (~acc) + 1;
-   }
-   if (endptr != 0) {
-      *endptr = (any >= 0 ? s - 1 : nptr);
-   }
+    if(endptr) {
+	*endptr = end;
+    }
 
-   if (ok != 0) {
-      *ok = any > 0;
-   }
-
-   return acc;
+    return retval;
 }
 
 #ifndef QT_QLOCALE_USES_FCVT
