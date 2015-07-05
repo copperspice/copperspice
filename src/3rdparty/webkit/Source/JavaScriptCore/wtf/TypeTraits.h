c1f2be7658490942408e1654c53f05e62fed5a1c
Ansel Sermersheim <ansel@copperspice.com>
2015-07-05 05:07:40 +0100

    Disable trivial destructor/constructor optimization for better compatibility

--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/TypeTraits.h
+++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/TypeTraits.h
@@ -180,54 +180,17 @@ namespace WTF {
 } // namespace WTF
 
 
-// copperspice solution for type traits (used in javascript and webkit)
-class cs_alternate{};
-
-class cs_preferred : public cs_alternate{};
-
-namespace std{
-   template<class> struct is_trivially_constructible;
-   template<class> struct has_trivial_default_constructor;
-
-   template<class> struct is_trivially_destructible;
-   template<class> struct has_trivial_destructor;
-}
 
 namespace WTF {
 
-// constructor
-template<typename T>
-decltype(std::is_trivially_constructible<T>::value, std::is_trivially_constructible<T>{})
-   cs_check_constructor(const cs_preferred &)
-{
-}
 
 template<typename T>
-decltype(std::has_trivial_default_constructor<T>::value, std::has_trivial_default_constructor<T>{})
-   cs_check_constructor(const cs_alternate &)
-{
-}
-
-template<typename T>
-struct HasTrivialConstructor : public decltype(cs_check_constructor<T>(cs_preferred{}))
+  struct HasTrivialConstructor : public std::false_type
 {
 };
 
-// destructor
 template<typename T>
-decltype(std::is_trivially_destructible<T>::value, std::is_trivially_destructible<T>{})
-   cs_check_destructor(const cs_preferred &)
-{
-}
-
-template<typename T>
-decltype(std::has_trivial_destructor<T>::value, std::has_trivial_destructor<T>{})
-   cs_check_destructor(const cs_alternate &)
-{
-}
-
-template<typename T>
-struct HasTrivialDestructor : public decltype(cs_check_destructor<T>(cs_preferred{}))
+  struct HasTrivialDestructor : public std::false_type
 {
 };
 
