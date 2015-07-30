60eedfe5fa2312ec3d5b5648fc0d5044d4879c6c
Ivailo Monev <xakepa10@gmail.com>
2015-07-30 14:56:21 +0300

    cmake build support for xmlpatterns

---
+++ src/xmlpatterns/janitors/janitors.cmake
@@ -0,0 +1,19 @@
+set(XMLPATTERNS_PRIVATE_INCLUDES
+    ${XMLPATTERNS_PRIVATE_INCLUDES}
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qargumentconverter_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qatomizer_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qcardinalityverifier_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qebvextractor_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qitemverifier_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/quntypedatomicconverter_p.h
+)
+
+set(XMLPATTERNS_SOURCES
+    ${XMLPATTERNS_SOURCES}
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qargumentconverter.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qatomizer.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qcardinalityverifier.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qebvextractor.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/qitemverifier.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/janitors/quntypedatomicconverter.cpp
+)
---
+++ src/xmlpatterns/projection/projection.cmake
@@ -0,0 +1,10 @@
+set(XMLPATTERNS_PRIVATE_INCLUDES
+    ${XMLPATTERNS_PRIVATE_INCLUDES}
+    ${CMAKE_CURRENT_SOURCE_DIR}/projection/qdocumentprojector_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/projection/qprojectedexpression_p.h
+)
+
+set(XMLPATTERNS_SOURCES
+    ${XMLPATTERNS_SOURCES}
+    ${CMAKE_CURRENT_SOURCE_DIR}/projection/qdocumentprojector.cpp
+)
