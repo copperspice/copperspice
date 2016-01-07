768c4c6a822a6eb4be390db768d59734ac51e195
Ansel Sermersheim <ansel@copperspice.com>
2016-01-06 20:24:56 -0800

    Small tweaks to CMake support

---
+++ cmake/modules/FindGOBJECT2.cmake
@@ -0,0 +1,52 @@
+# - Try to find the GOBJECT2 libraries
+# Once done this will define
+#
+#  GOBJECT2_FOUND - system has gobject2
+#  GOBJECT2_INCLUDE_DIR - the gobject2 include directory
+#  GOBJECT2_LIBRARIES - gobject2 library
+
+# Copyright (c) 2008 Laurent Montel, <montel@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+
+if(GOBJECT2_INCLUDE_DIR AND GOBJECT2_LIBRARIES)
+    # Already in cache, be silent
+    set(GOBJECT2_FIND_QUIETLY TRUE)
+endif(GOBJECT2_INCLUDE_DIR AND GOBJECT2_LIBRARIES)
+
+find_package(PkgConfig)
+pkg_check_modules(PC_LibGOBJECT2 QUIET gobject-2.0)
+
+find_path(GOBJECT2_MAIN_INCLUDE_DIR
+          NAMES gobject.h
+          HINTS ${PC_LibGOBJECT2_INCLUDEDIR}
+          PATH_SUFFIXES glib-2.0/gobject/)
+
+find_library(GOBJECT2_LIBRARY 
+             NAMES gobject-2.0 
+             HINTS ${PC_LibGOBJECT2_LIBDIR}
+)
+
+set(GOBJECT2_LIBRARIES ${GOBJECT2_LIBRARY})
+
+# search the gobjectconfig.h include dir under the same root where the library is found
+get_filename_component(gobject2LibDir "${GOBJECT2_LIBRARIES}" PATH)
+
+find_path(GOBJECT2_INTERNAL_INCLUDE_DIR glibconfig.h
+          PATH_SUFFIXES glib-2.0/include
+          HINTS ${PC_LibGOBJECT2_INCLUDEDIR} "${gobject2LibDir}" ${CMAKE_SYSTEM_LIBRARY_PATH})
+
+set(GOBJECT2_INCLUDE_DIR "${GOBJECT2_MAIN_INCLUDE_DIR}")
+
+# not sure if this include dir is optional or required
+# for now it is optional
+if(GOBJECT2_INTERNAL_INCLUDE_DIR)
+  set(GOBJECT2_INCLUDE_DIR ${GOBJECT2_INCLUDE_DIR} "${GOBJECT2_INTERNAL_INCLUDE_DIR}")
+endif(GOBJECT2_INTERNAL_INCLUDE_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GOBJECT2  DEFAULT_MSG  GOBJECT2_LIBRARIES GOBJECT2_MAIN_INCLUDE_DIR)
+
+mark_as_advanced(GOBJECT2_INCLUDE_DIR GOBJECT2_LIBRARIES)
