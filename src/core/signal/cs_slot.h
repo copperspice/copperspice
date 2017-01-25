4f2f41b4edd23cf564030b7a978cb073f88fecf5
Barbara Geller <barbara@copperspice.com>
2017-01-24 23:39:59 -0800

    Update BSD copyright

--- src/core/signal/cs_slot.h
+++ src/core/signal/cs_slot.h
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2015-2016 Barbara Geller
-* Copyright (c) 2015-2016 Ansel Sermersheim
+* Copyright (c) 2015-2017 Barbara Geller
+* Copyright (c) 2015-2017 Ansel Sermersheim
 * All rights reserved.
 *
 * This file is part of libCsSignal
