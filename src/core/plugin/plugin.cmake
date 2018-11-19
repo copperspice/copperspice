04c21356c3271e5ed904827177c7e1aa44f13c5a
Barbara Geller <barbara@copperspice.com>
2018-11-19 13:07:18 -0800

    Update core plugin build files

--- src/core/plugin/plugin.cmake
+++ src/core/plugin/plugin.cmake
@@ -4,8 +4,6 @@ set(CORE_PUBLIC_INCLUDES
     QLibrary
     QPluginLoader
     QUuid
-    QtPlugin
-    QtPluginInstanceFunction
 )
 
 set(CORE_INCLUDES
@@ -14,28 +12,27 @@ set(CORE_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qlibrary.h
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qplugin.h
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qpluginloader.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qtplugin.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qtplugininstancefunction.h
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/quuid.h
 )
 
 set(CORE_PRIVATE_INCLUDES
     ${CORE_PRIVATE_INCLUDES}
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qlibrary_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qfactoryloader_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qsystemlibrary_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qelfparser_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qlibrary_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qmachparser_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qsystemlibrary_p.h
 )
 set(CORE_SOURCES
     ${CORE_SOURCES}
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qpluginloader.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qfactoryloader.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/quuid.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qlibrary.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qelfparser_p.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qfactoryloader.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qfactoryinterface.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qlibrary.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qmachparser.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/qpluginloader.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/plugin/quuid.cpp
 )
-
-## FIXME platform-dependent
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     set(CORE_SOURCES
