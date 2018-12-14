a43e2f244e01c373784a17043e09a4ec25686728
Barbara Geller <barbara@copperspice.com>
2018-12-14 13:40:21 -0800

    Update graphics build files

--- src/gui/graphicsview/graphicsview.cmake
+++ src/gui/graphicsview/graphicsview.cmake
@@ -34,7 +34,6 @@ set(GUI_PUBLIC_INCLUDES
     QGraphicsTransform
     QGraphicsView
     QGraphicsWidget
-    QDeclarativeListProperty
 )
 
 set(GUI_INCLUDES
@@ -72,17 +71,19 @@ set(GUI_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicstextitem.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicstransform.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsview.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicswidget.h 
-    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qdeclarativelistproperty.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicswidget.h
 )
 
 set(GUI_PRIVATE_INCLUDES
     ${GUI_PRIVATE_INCLUDES}
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraph_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qdeclarativelistproperty_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsanchorlayout_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsgridlayoutengine_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsitem_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayout_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayoutitem_p.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayoutstyleinfo_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsproxywidget_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsscene_bsp_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsscene_p.h
@@ -101,10 +102,12 @@ set(GUI_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsanchorlayout.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsanchorlayout_p.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsgridlayout.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsgridlayoutengine.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsitem.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsitemanimation.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayout.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayout_p.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayoutstyleinfo.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslayoutitem.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicslinearlayout.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/graphicsview/qgraphicsproxywidget.cpp
