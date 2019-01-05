b896869fd69f4b887373cba805caf12f542b7b3e
Barbara Geller <barbara@copperspice.com>
2019-01-05 14:28:52 -0800

    Update build files for painting

--- src/gui/painting/painting.cmake
+++ src/gui/painting/painting.cmake
@@ -16,7 +16,6 @@ set(GUI_PUBLIC_INCLUDES
     QPaintEngineState
     QPainter
     QPainterPath
-    QPainterPathPrivate
     QPainterPathStroker
     QPen
     QPolygon
@@ -50,7 +49,6 @@ set(GUI_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintenginestate.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainter.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainterpath.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainterpathprivate.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainterpathstroker.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpen.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpolygon.h
@@ -74,11 +72,8 @@ set(GUI_PRIVATE_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcosmeticstroker_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcssutil_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdatabuffer_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_arm_simd_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_mmx_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_neon_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_sse_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_x86_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawingprimitive_sse2_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qemulationpaintengine_p.h
@@ -94,7 +89,6 @@ set(GUI_PRIVATE_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_preview_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_raster_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_x11_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengineex_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainter_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpainterpath_p.h
@@ -103,20 +97,12 @@ set(GUI_PRIVATE_INCLUDES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpen_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpolygonclipper_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprintengine_mac_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintbuffer_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprintengine_ps_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprinterinfo_unix_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qrasterdefs_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qrasterizer_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qregion_mac.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qstroker_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qtessellator_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qtextureglyphcache_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qunifiedtoolbarsurface_mac_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qvectorpath_p.h
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qrgba64_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qregion_x11.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qregion_win.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qwidgetbackingstore_p.h
 )
 
@@ -127,11 +113,13 @@ set(GUI_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qbrush.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolor.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolor_p.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolormap.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcosmeticstroker.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcssutil.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawutil.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_sse2.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_sse4.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper_ssse3.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qemulationpaintengine.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qmatrix.cpp
@@ -148,15 +136,12 @@ set(GUI_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpdf.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpen.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpolygon.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprintengine_ps.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qrasterizer.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qregion.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qstroker.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qstylepainter.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qtessellator.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qtextureglyphcache.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qtransform.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintbuffer.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_raster.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qdrawhelper.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qimagescale.cpp
@@ -167,23 +152,7 @@ set(GUI_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/painting/qwidgetbackingstore.cpp
 )
 
-if(X11_FOUND)
-    set(GUI_SOURCES
-        ${GUI_SOURCES}
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolormap_x11.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintdevice_x11.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_x11.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprinterinfo_unix.cpp
-    )
-endif()
-
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-    set(GUI_SOURCES
-        ${GUI_SOURCES}
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolormap_win.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintdevice_win.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprinterinfo_win.cpp
-    )
     set(EXTRA_GUI_LIBS
         ${EXTRA_GUI_LIBS}
         msimg32
@@ -192,11 +161,7 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(GUI_SOURCES
         ${GUI_SOURCES}
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qcolormap_mac.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintdevice_mac.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/painting/qpaintengine_mac.cpp
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprinterinfo_mac.cpp
         ${CMAKE_CURRENT_SOURCE_DIR}/painting/qprintengine_mac.mm
-        ${CMAKE_CURRENT_SOURCE_DIR}/painting/qunifiedtoolbarsurface_mac.cpp
     )
 endif()
