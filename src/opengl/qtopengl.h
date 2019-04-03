eacc30cc02e5d471ba9d4bceedf16429d0f052fb
Barbara Geller <barbara@copperspice.com>
2019-04-03 08:10:31 -0800

    Update master header files, add missing include files

---
+++ src/opengl/qtopengl.h
@@ -0,0 +1,12 @@
+#ifndef QT_OPENGL_H
+#define QT_OPENGL_H
+
+#include <qgl.h>
+#include <qglbuffer.h>
+#include <qglcolormap.h>
+#include <qglframebufferobject.h>
+#include <qglfunctions.h>
+#include <qglpixelbuffer.h>
+#include <qglshaderprogram.h>
+
+#endif
