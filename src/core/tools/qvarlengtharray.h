33cf6fae6fca9c905ade35c2986fffcda4694a1b
Barbara Geller <barbara@copperspice.com>
2019-04-16 13:12:03 -0800

    Rewrite QVarlengthArray class, add stl methods, reverse iterators, etc.

--- src/core/tools/qvarlengtharray.h
+++ src/core/tools/qvarlengtharray.h
@@ -24,16 +24,16 @@
 #ifndef QVARLENGTHARRAY_H
 #define QVARLENGTHARRAY_H
 
-#include <algorithm>
-
 #include <qcontainerfwd.h>
 #include <qglobal.h>
 #include <qassert.h>
 
+#include <algorithm>
+#include <initializer_list>
+#include <iterator>
 #include <new>
+#include <stdlib.h>
 #include <string.h>
-
-QT_BEGIN_NAMESPACE
 
 template<class T, int Prealloc>
 class QPodList;
@@ -53,12 +53,22 @@ class QVarLengthArray
 
    typedef T *iterator;
    typedef const T *const_iterator;
+   typedef std::reverse_iterator<iterator> reverse_iterator;
+   typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
    inline explicit QVarLengthArray(int size = 0);
 
    QVarLengthArray(const QVarLengthArray<T, Prealloc> &other)
       : a(Prealloc), s(0), ptr(reinterpret_cast<T *>(array)) {
       append(other.constData(), other.size());
+   }
+
+   QVarLengthArray(std::initializer_list<T> args)
+        : a(Prealloc), s(0), ptr(reinterpret_cast<T *>(array))
+   {
+      if (args.size()) {
+         append(args.begin(), args.size());
+      }
    }
 
    ~QVarLengthArray() {
@@ -68,12 +78,41 @@ class QVarLengthArray
             i->~T();
          }
       }
+
       if (ptr != reinterpret_cast<T *>(array)) {
-         qFree(ptr);
+         free(ptr);
       }
    }
 
    // methods
+   void append(const T *buf, int size);
+
+   void append(const T &t) {
+      if (s == a) {
+         // for example, s != 0
+         realloc(s, s << 1);
+      }
+      const int idx = s++;
+
+      if (QTypeInfo<T>::isComplex) {
+         new (ptr + idx) T(t);
+      } else {
+         ptr[idx] = t;
+      }
+   }
+
+   const T &at(int idx) const {
+      return operator[](idx);
+   }
+
+   T &back() {
+      return last();
+   }
+
+   const T &back() const {
+      return last();
+   }
+
    int capacity() const {
       return a;
    }
@@ -96,132 +135,160 @@ class QVarLengthArray
       return s;
    }
 
-   void removeLast() {
-      Q_ASSERT(s > 0);
-      realloc(s - 1, a);
+   T *data() {
+      return ptr;
    }
 
-   int size() const {
-      return s;
+   const T *data() const {
+      return ptr;
+   }
+
+   const T *constData() const {
+      return ptr;
+   }
+
+   bool empty() const {
+      return isEmpty();
+   }
+
+   iterator erase(const_iterator begin, const_iterator end);
+
+   iterator erase(const_iterator pos) {
+      return erase(pos, pos + 1);
+   }
+
+   T &first() {
+      Q_ASSERT(!isEmpty());
+      return *begin();
+   }
+
+   const T &first() const {
+      Q_ASSERT(!isEmpty());
+      return *begin();
+   }
+
+   T &front() {
+      return first();
+   }
+
+   const T &front() const {
+      return first();
+   }
+
+   T& last() {
+      Q_ASSERT(! isEmpty());
+      return *(end() - 1);
+   }
+
+   const T& last() const {
+      Q_ASSERT(!isEmpty());
+      return *(end() - 1);
+   }
+
+   int indexOf(const T &t, int from = 0) const;
+   int lastIndexOf(const T &t, int from = -1) const;
+
+   void insert(int i, const T &t);
+   void insert(int i, int n, const T &t);
+
+   iterator insert(const_iterator before, int n, const T &x);
+
+   iterator insert(const_iterator before, const T &x) {
+      return insert(before, 1, x);
    }
 
    bool isEmpty() const {
       return (s == 0);
    }
 
-   inline void resize(int size);
-   inline void reserve(int size);
+   int length() const {
+      return s;
+   }
 
-   const T &at(int idx) const {
-      return operator[](idx);
+   void prepend(const T &t);
+
+   void push_back(const T &t) {
+      append(t);
+   }
+
+   void pop_back() {
+      removeLast();
+   }
+
+   void replace(int i, const T &t);
+   void remove(int i);
+   void remove(int i, int n);
+   void removeLast() {
+      Q_ASSERT(s > 0);
+      realloc(s - 1, a);
+   }
+
+   void resize(int size);
+   void reserve(int size);
+
+   void squeeze();
+   int size() const {
+      return s;
    }
 
    T value(int i) const;
    T value(int i, const T &defaultValue) const;
 
-   void append(const T &t) {
-      if (s == a) { // i.e. s != 0
-         realloc(s, s << 1);
-      }
-      const int idx = s++;
-      if (QTypeInfo<T>::isComplex) {
-         new (ptr + idx) T(t);
-      } else {
-         ptr[idx] = t;
-      }
-   }
-
-   void append(const T *buf, int size);
-   QVarLengthArray<T, Prealloc> &operator<<(const T &t) {
-      append(t);
-      return *this;
-   }
-
-   QVarLengthArray<T, Prealloc> &operator+=(const T &t) {
-      append(t);
-      return *this;
-   }
-
-   void prepend(const T &t);
-   void insert(int i, const T &t);
-   void insert(int i, int n, const T &t);
-   void replace(int i, const T &t);
-   void remove(int i);
-   void remove(int i, int n);
-
-   iterator insert(iterator before, int n, const T &x);
-   inline iterator insert(iterator before, const T &x) {
-      return insert(before, 1, x);
-   }
-
-   iterator erase(iterator begin, iterator end);
-   inline iterator erase(iterator pos) {
-      return erase(pos, pos + 1);
-   }
-
-   inline bool empty() const {
-      return isEmpty();
-   }
-   inline void push_back(const T &t) {
-      append(t);
-   }
-   inline void pop_back() {
-      removeLast();
-   }
-   inline T &front() {
-      return *ptr;
-   }
-   inline const T &front() const {
-      return *ptr;
-   }
-   inline T &back() {
-      return *(ptr + s - 1);
-   }
-   inline const T &back() const {
-      return (*ptr + s - 1);
-   }
-
-   inline T *data() {
-      return ptr;
-   }
-   inline const T *data() const {
-      return ptr;
-   }
-   inline const T *constData() const {
-      return ptr;
-   }
-
    // iterators
-   inline iterator begin() {
+   iterator begin() {
       return ptr;
    }
 
-   inline const_iterator begin() const {
+   const_iterator begin() const {
       return ptr;
    }
 
-   inline const_iterator cbegin() const {
+   const_iterator cbegin() const {
       return ptr;
    }
 
-   inline const_iterator constBegin() const {
+   const_iterator constBegin() const {
       return ptr;
    }
 
-   inline iterator end() {
+   iterator end() {
       return ptr + s;
    }
 
-   inline const_iterator end() const {
+   const_iterator end() const {
       return ptr + s;
    }
 
-   inline const_iterator cend() const {
+   const_iterator cend() const {
       return ptr + s;
    }
 
-   inline const_iterator constEnd() const {
+   const_iterator constEnd() const {
       return ptr + s;
+   }
+
+   reverse_iterator rbegin() {
+      return reverse_iterator(end());
+   }
+
+   reverse_iterator rend() {
+      return reverse_iterator(begin());
+   }
+
+   const_reverse_iterator rbegin() const {
+      return const_reverse_iterator(end());
+   }
+
+   const_reverse_iterator rend() const {
+      return const_reverse_iterator(begin());
+   }
+
+   const_reverse_iterator crbegin() const {
+      return const_reverse_iterator(end());
+   }
+
+   const_reverse_iterator crend() const {
+      return const_reverse_iterator(begin());
    }
 
    // operators
@@ -233,7 +300,15 @@ class QVarLengthArray
       return *this;
    }
 
+   QVarLengthArray<T, Prealloc> &operator=(std::initializer_list<T> list)
+   {
+      resize(list.size());
+      std::copy(list.begin(), list.end(), this->begin());
 
+      return *this;
+   }
+
+   // operators
    T &operator[](int idx) {
       Q_ASSERT(idx >= 0 && idx < s);
       return ptr[idx];
@@ -244,6 +319,16 @@ class QVarLengthArray
       return ptr[idx];
    }
 
+   QVarLengthArray<T, Prealloc> &operator<<(const T &t) {
+      append(t);
+      return *this;
+   }
+
+   QVarLengthArray<T, Prealloc> &operator+=(const T &t) {
+      append(t);
+      return *this;
+   }
+
  private:
    friend class QPodList<T, Prealloc>;
    void realloc(int size, int alloc);
@@ -251,26 +336,27 @@ class QVarLengthArray
    int a;      // capacity
    int s;      // size
    T *ptr;     // data
+
    union {
-      // ### Qt5/Use 'Prealloc * sizeof(T)' as array size
-      char array[sizeof(qint64) * (((Prealloc *sizeof(T)) / sizeof(qint64)) + 1)];
+      char array[Prealloc * sizeof(T)];
       qint64 q_for_alignment_1;
       double q_for_alignment_2;
    };
 };
 
 template <class T, int Prealloc>
-inline QVarLengthArray<T, Prealloc>::QVarLengthArray(int asize)
+QVarLengthArray<T, Prealloc>::QVarLengthArray(int asize)
    : s(asize)
 {
    if (s > Prealloc) {
-      ptr = reinterpret_cast<T *>(qMalloc(s * sizeof(T)));
+      ptr = reinterpret_cast<T *>(malloc(s * sizeof(T)));
       Q_CHECK_PTR(ptr);
       a = s;
    } else {
       ptr = reinterpret_cast<T *>(array);
       a = Prealloc;
    }
+
    if (QTypeInfo<T>::isComplex) {
       T *i = ptr + s;
       while (i != ptr) {
@@ -280,17 +366,36 @@ inline QVarLengthArray<T, Prealloc>::QVarLengthArray(int asize)
 }
 
 template <class T, int Prealloc>
-inline void QVarLengthArray<T, Prealloc>::resize(int asize)
+int QVarLengthArray<T, Prealloc>::indexOf(const T &t, int from) const
 {
-   realloc(asize, qMax(asize, a));
+    if (from < 0)
+        from = qMax(from + s, 0);
+    if (from < s) {
+        T *n = ptr + from - 1;
+        T *e = ptr + s;
+        while (++n != e)
+            if (*n == t)
+                return n - ptr;
+    }
+    return -1;
 }
 
 template <class T, int Prealloc>
-inline void QVarLengthArray<T, Prealloc>::reserve(int asize)
+int QVarLengthArray<T, Prealloc>::lastIndexOf(const T &t, int from) const
 {
-   if (asize > a) {
-      realloc(s, asize);
-   }
+    if (from < 0)
+        from += s;
+    else if (from >= s)
+        from = s - 1;
+    if (from >= 0) {
+        T *b = ptr;
+        T *n = ptr + from + 1;
+        while (n != b) {
+            if (*--n == t)
+                return n - b;
+        }
+    }
+    return -1;
 }
 
 template <class T, int Prealloc>
@@ -312,6 +417,7 @@ void QVarLengthArray<T, Prealloc>::append(const T *abuf, int increment)
       while (s < asize) {
          new (ptr + (s++)) T(*abuf++);
       }
+
    } else {
       memcpy(&ptr[s], abuf, increment * sizeof(T));
       s = asize;
@@ -319,43 +425,64 @@ void QVarLengthArray<T, Prealloc>::append(const T *abuf, int increment)
 }
 
 template <class T, int Prealloc>
+void QVarLengthArray<T, Prealloc>::resize(int asize)
+{
+   realloc(asize, qMax(asize, a));
+}
+
+template <class T, int Prealloc>
+void QVarLengthArray<T, Prealloc>::reserve(int asize)
+{
+   if (asize > a) {
+      realloc(s, asize);
+   }
+}
+
+template <class T, int Prealloc>
 void QVarLengthArray<T, Prealloc>::realloc(int asize, int aalloc)
 {
    Q_ASSERT(aalloc >= asize);
+
    T *oldPtr = ptr;
    int osize = s;
 
    const int copySize = qMin(asize, osize);
    if (aalloc != a) {
-      ptr = reinterpret_cast<T *>(qMalloc(aalloc * sizeof(T)));
+      ptr = reinterpret_cast<T *>(malloc(aalloc * sizeof(T)));
       Q_CHECK_PTR(ptr);
+
       if (ptr) {
          s = 0;
          a = aalloc;
 
          if (QTypeInfo<T>::isStatic) {
-            QT_TRY {
+            try {
                // copy all the old elements
-               while (s < copySize)
-               {
+               while (s < copySize) {
                   new (ptr + s) T(*(oldPtr + s));
                   (oldPtr + s)->~T();
                   s++;
                }
-            } QT_CATCH(...) {
+
+            } catch(...) {
                // clean up all the old objects and then free the old ptr
                int sClean = s;
+
                while (sClean < osize) {
                   (oldPtr + (sClean++))->~T();
                }
+
                if (oldPtr != reinterpret_cast<T *>(array) && oldPtr != ptr) {
-                  qFree(oldPtr);
+                  free(oldPtr);
                }
-               QT_RETHROW;
+
+               throw;
             }
+
          } else {
             memcpy(ptr, oldPtr, copySize * sizeof(T));
          }
+
       } else {
          ptr = oldPtr;
          return;
@@ -371,7 +498,7 @@ void QVarLengthArray<T, Prealloc>::realloc(int asize, int aalloc)
    }
 
    if (oldPtr != reinterpret_cast<T *>(array) && oldPtr != ptr) {
-      qFree(oldPtr);
+      free(oldPtr);
    }
 
    if (QTypeInfo<T>::isComplex) {
@@ -382,6 +509,12 @@ void QVarLengthArray<T, Prealloc>::realloc(int asize, int aalloc)
    } else {
       s = asize;
    }
+}
+
+template <class T, int Prealloc>
+void QVarLengthArray<T, Prealloc>::squeeze()
+{
+   realloc(s, s);
 }
 
 template <class T, int Prealloc>
@@ -442,24 +575,28 @@ inline void QVarLengthArray<T, Prealloc>::replace(int i, const T &t)
 }
 
 template <class T, int Prealloc>
-typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::insert(
-   iterator before, size_type n, const T &t)
+typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::insert(const_iterator before, size_type n, const T &t)
 {
    int offset = int(before - ptr);
    if (n != 0) {
       resize(s + n);
       const T copy(t);
+
       if (QTypeInfo<T>::isStatic) {
          T *b = ptr + offset;
          T *j = ptr + s;
          T *i = j - n;
+
          while (i != b) {
             *--j = *--i;
          }
+
          i = b + n;
+
          while (i != b) {
             *--i = copy;
          }
+
       } else {
          T *b = ptr + offset;
          T *i = b + n;
@@ -473,8 +610,7 @@ typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::in
 }
 
 template <class T, int Prealloc>
-typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::erase(
-   iterator abegin, iterator aend)
+typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::erase(const_iterator abegin, const_iterator aend)
 {
    int f = int(abegin - ptr);
    int l = int(aend - ptr);
@@ -482,16 +618,21 @@ typename QVarLengthArray<T, Prealloc>::iterator QVarLengthArray<T, Prealloc>::er
 
    if (QTypeInfo<T>::isComplex) {
       std::copy(ptr + l, ptr + s, ptr + f);
+
       T *i = ptr + s;
       T *b = ptr + s - n;
+
       while (i != b) {
          --i;
          i->~T();
       }
+
    } else {
       memmove(ptr + f, ptr + l, (s - l) * sizeof(T));
    }
+
    s -= n;
+
    return ptr + f;
 }
 
@@ -501,13 +642,16 @@ bool operator==(const QVarLengthArray<T, Prealloc1> &l, const QVarLengthArray<T,
    if (l.size() != r.size()) {
       return false;
    }
+
    for (int i = 0; i < l.size(); i++) {
       if (l.at(i) != r.at(i)) {
          return false;
       }
    }
+
    return true;
 }
+
 
 template <typename T, int Prealloc1, int Prealloc2>
 bool operator!=(const QVarLengthArray<T, Prealloc1> &l, const QVarLengthArray<T, Prealloc2> &r)
@@ -515,6 +659,4 @@ bool operator!=(const QVarLengthArray<T, Prealloc1> &l, const QVarLengthArray<T,
    return !(l == r);
 }
 
-QT_END_NAMESPACE
-
-#endif // QVARLENGTHARRAY_H
+#endif
