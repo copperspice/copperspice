c8f6d048c5687fe4879a06b329bf7195b1931aaf
Barbara Geller <barbara@copperspice.com>
2019-05-01 14:20:18 -0800

    Add files to support FontConfig

---
+++ src/plugins/platforms/xcb/fonts/qfontconfigdatabase_p.h
@@ -0,0 +1,48 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2019 Barbara Geller
+* Copyright (c) 2012-2019 Ansel Sermersheim
+*
+* Copyright (C) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#ifndef QFONTCONFIGDATABASE_P_H
+#define QFONTCONFIGDATABASE_P_H
+
+#include <qplatform_fontdatabase.h>
+#include <qbasicfontdatabase_p.h>
+
+class QFontEngineFT;
+
+class QFontconfigDatabase : public QBasicFontDatabase
+{
+public:
+    void populateFontDatabase() override;
+    QFontEngineMulti *fontEngineMulti(QFontEngine *fontEngine, QChar::Script script) override;
+    QFontEngine *fontEngine(const QFontDef &fontDef, void *handle) override;
+    QFontEngine *fontEngine(const QByteArray &fontData, qreal pixelSize, QFont::HintingPreference hintingPreference) override;
+    QStringList fallbacksForFamily(const QString &family, QFont::Style style, QFont::StyleHint styleHint, QChar::Script script) const override;
+    QStringList addApplicationFont(const QByteArray &fontData, const QString &fileName) override;
+    QString resolveFontFamilyAlias(const QString &family) const override;
+    QFont defaultFont() const override;
+
+private:
+    void setupFontEngine(QFontEngineFT *engine, const QFontDef &fontDef) const;
+};
+
+#endif
---
+++ src/plugins/platforms/xcb/fonts/qfontenginemultifontconfig.cpp
@@ -0,0 +1,86 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2019 Barbara Geller
+* Copyright (c) 2012-2019 Ansel Sermersheim
+*
+* Copyright (C) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#include <qfontenginemultifontconfig_p.h>
+
+#include <qfontengine_ft_p.h>
+
+QFontEngineMultiFontConfig::QFontEngineMultiFontConfig(QFontEngine *fe, int script)
+   : QFontEngineMulti(fe, script)
+{
+}
+
+QFontEngineMultiFontConfig::~QFontEngineMultiFontConfig()
+{
+   for (FcPattern *pattern : cachedMatchPatterns) {
+      if (pattern) {
+         FcPatternDestroy(pattern);
+      }
+   }
+}
+
+bool QFontEngineMultiFontConfig::shouldLoadFontEngineForCharacter(int at, char32_t ch) const
+{
+   bool charSetHasChar = true;
+   FcPattern *matchPattern = getMatchPatternForFallback(at - 1);
+
+   if (matchPattern != 0) {
+      FcCharSet *charSet;
+      FcPatternGetCharSet(matchPattern, FC_CHARSET, 0, &charSet);
+      charSetHasChar = FcCharSetHasChar(charSet, ch);
+   }
+
+   return charSetHasChar;
+}
+
+FcPattern *QFontEngineMultiFontConfig::getMatchPatternForFallback(int fallBackIndex) const
+{
+   Q_ASSERT(fallBackIndex < fallbackFamilyCount());
+
+   if (fallbackFamilyCount() > cachedMatchPatterns.size()) {
+      cachedMatchPatterns.resize(fallbackFamilyCount());
+   }
+
+   FcPattern *retval = cachedMatchPatterns.at(fallBackIndex);
+
+   if (retval) {
+      return retval;
+   }
+
+   FcPattern *requestPattern = FcPatternCreate();
+   FcValue value;
+   value.type = FcTypeString;
+
+   QByteArray cs = fallbackFamilyAt(fallBackIndex).toUtf8();
+   value.u.s = reinterpret_cast<const FcChar8 *>(cs.data());
+   FcPatternAdd(requestPattern, FC_FAMILY, value, true);
+
+   FcResult result;
+   retval = FcFontMatch(0, requestPattern, &result);
+   cachedMatchPatterns.insert(fallBackIndex, retval);
+   FcPatternDestroy(requestPattern);
+
+   return retval;
+}
+
+
