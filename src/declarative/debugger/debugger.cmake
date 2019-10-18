32653f447e7c9279942093c3b5c8301494fadeb0
Barbara Geller <barbara@copperspice.com>
2019-10-18 09:18:10 -0800

    Modernize various CMake build files

--- src/declarative/debugger/debugger.cmake
+++ src/declarative/debugger/debugger.cmake
@@ -1,45 +1,42 @@
-set(DECLARATIVE_PUBLIC_INCLUDES
-    ${DECLARATIVE_PUBLIC_INCLUDES}
-    QDeclarativeDebuggingEnabler
+list(APPEND DECLARATIVE_PUBLIC_INCLUDES
+   QDeclarativeDebuggingEnabler
 )
 
-set(DECLARATIVE_INCLUDES
-    ${DECLARATIVE_INCLUDES}
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebug.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggingenabler.h 
+list(APPEND DECLARATIVE_INCLUDES
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebug.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggingenabler.h
 )
 
-set(DECLARATIVE_PRIVATE_INCLUDES
-    ${DECLARATIVE_PRIVATE_INCLUDES}
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggerstatus_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qpacketprotocol_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice_p_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugclient_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebug_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugtrace_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebughelper_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserver_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserverconnection_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorservice_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorinterface_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebuggeragent_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebugservice_p.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebugservice_p.h
+list(APPEND DECLARATIVE_PRIVATE_INCLUDES
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggerstatus_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qpacketprotocol_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice_p_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugclient_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebug_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugtrace_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebughelper_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserver_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserverconnection_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorservice_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorinterface_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebuggeragent_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebugservice_p.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebugservice_p.h
 )
 
-set(DECLARATIVE_SOURCES
-    ${DECLARATIVE_SOURCES}
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggerstatus.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qpacketprotocol.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugclient.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebug.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugtrace.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebughelper.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserver.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorservice.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebuggeragent.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebugservice.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebugservice.cpp
+target_sources(CsDeclarative
+   PRIVATE
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebuggerstatus.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qpacketprotocol.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugservice.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugclient.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebug.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugtrace.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebughelper.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativedebugserver.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeinspectorservice.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebuggeragent.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qjsdebugservice.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/debugger/qdeclarativeenginedebugservice.cpp
 )
