63d54789f3636ead55403b739d6a849f247a2b58
Barbara Geller <barbara@copperspice.com>
2020-01-04 13:03:16 -0800

    Update copyright for various libraries

--- src/declarative/qml/qdeclarativevme.cpp
+++ src/declarative/qml/qdeclarativevme.cpp
@@ -1,9 +1,9 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2019 Barbara Geller
-* Copyright (c) 2012-2019 Ansel Sermersheim
+* Copyright (c) 2012-2020 Barbara Geller
+* Copyright (c) 2012-2020 Ansel Sermersheim
 *
-* Copyright (C) 2015 The Qt Company Ltd.
+* Copyright (c) 2015 The Qt Company Ltd.
 * Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
 * Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
 *
@@ -99,8 +99,6 @@ struct ListInstance {
    int type;
    QDeclarativeListProperty<void> qListProperty;
 };
-
-Q_DECLARE_TYPEINFO(ListInstance, Q_PRIMITIVE_TYPE  | Q_MOVABLE_TYPE);
 
 QObject *QDeclarativeVME::run(QDeclarativeContextData *ctxt, QDeclarativeCompiledData *comp,
                               int start, int count, const QBitField &bindingSkipList)
