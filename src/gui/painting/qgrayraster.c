76e17933255a368ea69b09f184ec55eab1757075
Barbara Geller <barbara@copperspice.com>
2020-01-04 16:27:11 -0800

    Update copyright for CsGui

--- src/gui/painting/qgrayraster.c
+++ src/gui/painting/qgrayraster.c
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2019 Barbara Geller
-* Copyright (c) 2012-2019 Ansel Sermersheim
+* Copyright (c) 2012-2020 Barbara Geller
+* Copyright (c) 2012-2020 Ansel Sermersheim
 *
 * Copyright (C) 2015 The Qt Company Ltd.
 * Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
