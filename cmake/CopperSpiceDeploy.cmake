bd2df468f2b25a7821738a7940838d359699c33f
Ansel Sermersheim <ansel@copperspice.com>
2020-04-06 09:17:14 -0800

    Change CMake so files are installed correctly in a Mac bundle

--- cmake/CopperSpiceDeploy.cmake
+++ cmake/CopperSpiceDeploy.cmake
@@ -30,8 +30,8 @@ function(cs_copy_library LIB_NAME)
    # location of the cs library
    get_target_property(CS_${LIB_NAME}_LIB CopperSpice::${LIB_NAME} LOCATION)
 
-   if(ARGN EQUAL 1)
-      set(APP_INSTALL_DIR ${ARG0})
+   if(ARGC EQUAL 2)
+      set(APP_INSTALL_DIR ${ARGV1})
    else()
       set(APP_INSTALL_DIR .)
    endif()
@@ -42,8 +42,8 @@ endfunction()
 
 function(cs_copy_plugins LIB_NAME)
 
-   if(ARGN EQUAL 1)
-      set(APP_INSTALL_DIR ${ARG0})
+   if(ARGC EQUAL 2)
+      set(APP_INSTALL_DIR ${ARGV1})
    else()
       set(APP_INSTALL_DIR .)
    endif()
@@ -51,13 +51,13 @@ function(cs_copy_plugins LIB_NAME)
    if(LIB_NAME STREQUAL "CsGui")
 
       if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-         install(FILES ${CS_PLUGIN_DIR}/CsGuiCocoa1.6.so DESTINATION ${APP_INSTALL_DIR}/platforms)
+         install(FILES ${CS_PLUGIN_DIR}/CsGuiCocoa${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/platforms)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "(Linux|OpenBSD|FreeBSD|NetBSD|DragonFly)")
-         install(FILES ${CS_PLUGIN_DIR}/CsGuiXcb1.6.so DESTINATION ${APP_INSTALL_DIR}/platforms)
+         install(FILES ${CS_PLUGIN_DIR}/CsGuiXcb${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/platforms)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-         install(FILES ${CS_PLUGIN_DIR}/CsGuiWin1.6.dll DESTINATION ${APP_INSTALL_DIR}/platforms)
+         install(FILES ${CS_PLUGIN_DIR}/CsGuiWin${COPPERSPICE_VERSION_API}.dll DESTINATION ${APP_INSTALL_DIR}/platforms)
 
       endif()
    endif()
@@ -65,17 +65,17 @@ function(cs_copy_plugins LIB_NAME)
    if(LIB_NAME STREQUAL "CsMultimedia")
 
       if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_avf_mediaplayer1.6.so DESTINATION ${APP_INSTALL_DIR}/mediaservices)
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u1.6.so DESTINATION ${APP_INSTALL_DIR}/playlistformats)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_avf_mediaplayer${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/mediaservices)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/playlistformats)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "(Linux|OpenBSD|FreeBSD|NetBSD|DragonFly)")
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_gst_mediaplayer1.6.so DESTINATION ${APP_INSTALL_DIR}/mediaservices)
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_gst_audiodecoder1.6.so DESTINATION ${APP_INSTALL_DIR}/audio)
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u1.6.so DESTINATION ${APP_INSTALL_DIR}/playlistformats)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_gst_mediaplayer${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/mediaservices)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_gst_audiodecoder${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/audio)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/playlistformats)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_DirectShow1.6.dll DESTINATION ${APP_INSTALL_DIR}/mediaservices)
-         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u1.6.dll DESTINATION ${APP_INSTALL_DIR}/playlistformats)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_DirectShow${COPPERSPICE_VERSION_API}.dll DESTINATION ${APP_INSTALL_DIR}/mediaservices)
+         install(FILES ${CS_PLUGIN_DIR}/CsMultimedia_m3u${COPPERSPICE_VERSION_API}.dll DESTINATION ${APP_INSTALL_DIR}/playlistformats)
 
       endif()
    endif()
@@ -83,13 +83,13 @@ function(cs_copy_plugins LIB_NAME)
    if(LIB_NAME STREQUAL "CsSqlPsql")
 
       if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql1.6.so DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
+         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "(Linux|OpenBSD|FreeBSD|NetBSD|DragonFly)")
-         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql1.6.so DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
+         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql${COPPERSPICE_VERSION_API}.so DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql1.6.dll DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
+         install(FILES ${CS_PLUGIN_DIR}/CsSqlPsql${COPPERSPICE_VERSION_API}.dll DESTINATION ${APP_INSTALL_DIR}/sqldrivers)
 
       endif()
    endif()
@@ -97,13 +97,13 @@ function(cs_copy_plugins LIB_NAME)
    if(LIB_NAME STREQUAL "CsPrinterDriver")
 
       if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverCups1.6.so)
+         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverCups${COPPERSPICE_VERSION_API}.so)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "(Linux|OpenBSD|FreeBSD|NetBSD|DragonFly)")
-         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverCups1.6.so)
+         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverCups${COPPERSPICE_VERSION_API}.so)
 
       elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverWin1.6.dll)
+         set(file ${CS_PLUGIN_DIR}/CsPrinterDriverWin${COPPERSPICE_VERSION_API}.dll)
 
       endif()
 
