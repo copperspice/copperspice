d04aec48011192f188f71a535eb8fdf51509a2d4
Barbara Geller <barbara@copperspice.com>
2020-08-04 14:35:02 -0800

    Add Xcb_Support plugin build file, rework Xcb plugin

--- src/plugins/platforms/xcb/xcb.cmake
+++ src/plugins/platforms/xcb/xcb.cmake
@@ -1,140 +1,32 @@
 list(APPEND PLATFORMS_XCB_PRIVATE_INCLUDES
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbclipboard.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbconnection.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbintegration.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbkeyboard.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbdrag.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbexport.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbmime.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbobject.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbscreen.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbwindow.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbbackingstore.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbwmsupport.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbnativeinterface.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbcursor.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbimage.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbxsettings.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbsystemtraytracker.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbsessionmanager.h
-)
-
-list(APPEND PLATFORMS_XCB_OTHER_PRIVATE_INCLUDES
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbglintegration.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbglintegrationfactory.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbglintegrationplugin.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbnativeinterfacehandler.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunixeventdispatcher_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunixservices_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qunixeventdispatcher_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qxcbeventdispatcher_glib_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontconfigdatabase_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontenginemultifontconfig_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qgenericunixfontdatabase_p.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/xcb/themes/qgenericunixthemes_p.h
 )
 
 if(BUILD_PLATFORMS_XCB_PLUGIN)
-
    add_library(CsGuiXcb MODULE "")
    add_library(CopperSpice::CsGuiXcb ALIAS CsGuiXcb)
 
-   set_target_properties(CsGuiXcb PROPERTIES OUTPUT_NAME CsGuiXcb${BUILD_ABI} PREFIX "")
+   set_target_properties(CsGuiXcb PROPERTIES
+      OUTPUT_NAME   CsGuiXcb${BUILD_ABI} PREFIX ""
+      INSTALL_RPATH "$ORIGIN/.."
+   )
 
    target_sources(CsGuiXcb
       PRIVATE
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbmain.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbclipboard.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbconnection.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbintegration.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbkeyboard.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbmime.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbdrag.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbscreen.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbwindow.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbbackingstore.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbwmsupport.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbnativeinterface.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbcursor.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbimage.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbxsettings.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbsystemtraytracker.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbsessionmanager.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcbconnection_xi2.cpp
-
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbglintegrationfactory.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbglintegration.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xgl/qxcbnativeinterfacehandler.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunixeventdispatcher.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunixservices.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qunixeventdispatcher.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontconfigdatabase.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontenginemultifontconfig.cpp
-      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/themes/qgenericunixthemes.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/qxcb_main.cpp
    )
-
-   if(GTK2_FOUND)
-      target_sources(CsGuiXcb
-         PRIVATE
-         ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qxcbeventdispatcher_glib.cpp
-      )
-   endif()
 
    target_link_libraries(CsGuiXcb
       PRIVATE
       CsCore
       CsGui
-      ${FONTCONFIG_LIBRARIES}
-      ${XCB_LIB}
-      ${XCB_IMAGE_LIB}
-      ${XCB_ICCCM_LIB}
-      ${XCB_SYNC_LIB}
-      ${XCB_XFIXES_LIB}
-      ${XCB_SHM_LIB}
-      ${XCB_RANDR_LIB}
-      ${XCB_SHAPE_LIB}
-      ${XCB_KEYSYMS_LIB}
-      ${XCB_XINERAMA_LIB}
-      ${XCB_XKB_LIB}
-      ${XCB_RENDER_LIB}
-      ${XCB_RENDER_UTIL_LIB}
-      ${X11_SM_LIB}
-      ${X11_ICE_LIB}
-      ${X11_X11_LIB}
-      ${X11_Xi_LIB}
-      ${X11_XCB_LIB}
-      ${XKBCOMMON_LIB}
-      ${XKBCOMMON_X11_LIB}
-   )
-
-   target_include_directories(CsGuiXcb
-      PRIVATE
-      ${CMAKE_SOURCE_DIR}/src/3rdparty/freetype/include
-      ${CMAKE_SOURCE_DIR}/src/3rdparty/freetype/include/freetype
-      ${CMAKE_SOURCE_DIR}/src/3rdparty/harbuzz/src
+      CsXcbSupport
    )
 
    target_compile_definitions(CsGuiXcb
       PRIVATE
       -DQT_PLUGIN
-      -DQ_FONTCONFIGDATABASE
-      -DQT_USE_FONTCONFIG
-      -DQT_USE_FREETYPE
-      -DHAVE_POSIX_MEMALIGN
-      -DQT_NO_ACCESSIBILITY_ATSPI_BRIDGE
-      -DQT_NO_DBUS
-      -DXCB_USE_RENDER
-      -DXCB_USE_SM
-      -DXCB_USE_XLIB
       -DXCB_USE_XINPUT2
    )
-
-   if(NOT GTK2_FOUND)
-      target_compile_definitions(CsGuiXcb
-         PRIVATE
-         -DQT_NO_GLIB
-      )
-   endif()
 
    install(TARGETS CsGuiXcb DESTINATION ${CMAKE_INSTALL_LIBDIR})
 endif()
---
+++ src/plugins/platforms/xcb/xcb_support/xcb_support.cmake
@@ -0,0 +1,157 @@
+if(BUILD_PLATFORMS_XCB_PLUGIN)
+   add_library(CsXcbSupport SHARED "")
+   add_library(CopperSpice::CsXcbSupport ALIAS CsXcbSupport)
+
+   set_target_properties(CsXcbSupport PROPERTIES OUTPUT_NAME CsXcbSupport${BUILD_ABI})
+
+   target_compile_definitions(CsXcbSupport
+      PRIVATE
+      -DQT_NO_ACCESSIBILITY_ATSPI_BRIDGE
+      -DQT_NO_DBUS
+      -DQT_FONTCONFIGDATABASE
+      -DQT_USE_FREETYPE
+      -DXCB_USE_RENDER
+      -DXCB_USE_SM
+      -DXCB_USE_XLIB
+      -DXCB_USE_XINPUT2
+   )
+
+   list(APPEND XCB_SUPPORT_PRIVATE_INCLUDES
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_backingstore.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_clipboard.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_connection.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_cursor.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_drag.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_export.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_gl_integration.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_gl_integrationfactory.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_gl_integrationplugin.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_image.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_integration.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_keyboard.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_mime.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_nativeinterface.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_nativeinterfacehandler.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_object.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_screen.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_sessionmanager.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_systemtraytracker.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_window.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_wm_support.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_xsettings.h
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunix_eventdispatcher_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qunix_eventdispatcher_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qxcb_eventdispatcher_glib_p.h
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontconfig_database_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontengine_multifontconfig_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qgenericunix_fontdatabase_p.h
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/services/qgenericunix_services_p.h
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/themes/qgenericunix_theme_p.h
+   )
+
+   target_sources(CsXcbSupport
+      PRIVATE
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_backingstore.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_clipboard.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_connection.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_connection_xi2.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_cursor.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_drag.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_gl_integration.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_gl_integrationfactory.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_image.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_integration.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_keyboard.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_mime.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_nativeinterface.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_nativeinterfacehandler.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_screen.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_sessionmanager.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_systemtraytracker.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_window.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_wm_support.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/xcb_support/qxcb_xsettings.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qgenericunix_eventdispatcher.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qunix_eventdispatcher.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontconfig_database.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/fonts/qfontengine_multifontconfig.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/services/qgenericunix_services.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/xcb/themes/qgenericunix_theme.cpp
+   )
+
+   macro_generate_misc_private("${XCB_SUPPORT_PRIVATE_INCLUDES}" QtGui/private/platforms)
+
+   target_sources(CsXcbSupport
+      PRIVATE
+      ${XCB_SUPPORT_PRIVATE_INCLUDES}
+   )
+
+   target_link_libraries(CsXcbSupport
+      PRIVATE
+      CsCore
+      CsGui
+      ${FONTCONFIG_LIBRARIES}
+      ${XCB_LIB}
+      ${XCB_IMAGE_LIB}
+      ${XCB_ICCCM_LIB}
+      ${XCB_SYNC_LIB}
+      ${XCB_XFIXES_LIB}
+      ${XCB_SHM_LIB}
+      ${XCB_RANDR_LIB}
+      ${XCB_SHAPE_LIB}
+      ${XCB_KEYSYMS_LIB}
+      ${XCB_XINERAMA_LIB}
+      ${XCB_XKB_LIB}
+      ${XCB_RENDER_LIB}
+      ${XCB_RENDER_UTIL_LIB}
+      ${X11_SM_LIB}
+      ${X11_ICE_LIB}
+      ${X11_X11_LIB}
+      ${X11_Xi_LIB}
+      ${X11_XCB_LIB}
+      ${XKBCOMMON_LIB}
+      ${XKBCOMMON_X11_LIB}
+   )
+
+   target_include_directories(CsXcbSupport
+      PRIVATE
+      ${CMAKE_SOURCE_DIR}/src/3rdparty/freetype/include
+      ${CMAKE_SOURCE_DIR}/src/3rdparty/freetype/include/freetype
+      ${CMAKE_SOURCE_DIR}/src/3rdparty/harbuzz/src
+   )
+
+   if(GTK2_FOUND)
+      target_link_libraries(CsXcbSupport
+         PRIVATE
+         ${GLIB2_LIBRARIES}
+         ${GOBJECT2_LIBRARIES}
+      )
+
+      target_sources(CsGuiXcb
+         PRIVATE
+         ${CMAKE_CURRENT_SOURCE_DIR}/xcb/events/qxcb_eventdispatcher_glib.cpp
+      )
+
+   else()
+      target_compile_definitions(CsXcbSupport
+         PRIVATE
+         -DQT_NO_GLIB
+      )
+   endif()
+
+   install(
+      TARGETS CsXcbSupport
+      EXPORT CopperSpiceLibraryTargets ${INSTALL_TARGETS_DEFAULT_ARGS}
+      RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+      LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+      ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+   )
+endif()
