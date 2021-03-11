6ab10bef7961540f1478b7d1ab8aa163d3cec6b4
Ansel Sermersheim <ansel@copperspice.com>
2021-03-11 14:37:25 -0800

    Add new QTranslator catch tests

---
+++ test/core/kernel/qtranslator.cpp
@@ -0,0 +1,64 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2021 Barbara Geller
+* Copyright (c) 2012-2021 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#include <qtranslator.h>
+
+#include <qfile.h>
+#include <qbytearray.h>
+
+#include <cs_catch2.h>
+
+static const QString qmPath = ":/test_translator";
+
+TEST_CASE("QTranslator load_a", "[qtranslator]")
+{
+   QTranslator obj;
+   obj.load(":/test_translator/test_de");
+
+   REQUIRE(obj.isEmpty() == false);
+   REQUIRE(obj.translate("QPushButton", "Open") == "Öffnen");
+   REQUIRE(obj.translate("QPushButton", "Close") == "Schließen");
+}
+
+TEST_CASE("QTranslator load_b", "[qtranslator]")
+{
+   QTranslator obj;
+
+   QFile file(":/test_translator/test_de.qm");
+   file.open(QFile::ReadOnly);
+
+   QByteArray data = file.readAll();
+   obj.load((const uchar *)data.constData(), data.length());
+
+   REQUIRE(obj.isEmpty() == false);
+   REQUIRE(obj.translate("QPushButton", "Open") == "Öffnen");
+   REQUIRE(obj.translate("QPushButton", "Close") == "Schließen");
+}
+
+TEST_CASE("QTranslator plural", "[qtranslator]")
+{
+   QTranslator obj;
+   obj.load(":/test_translator/test_de");
+
+   REQUIRE(obj.isEmpty() == false);
+
+   REQUIRE(obj.translate("QThing", "Attending %n conference(s)", nullptr, 0) == "Teilnahme an 0 Konferenzen");
+   REQUIRE(obj.translate("QThing", "Attending %n conference(s)", nullptr, 1) == "Teilnahme an 1 Konferenz");
+   REQUIRE(obj.translate("QThing", "Attending %n conference(s)", nullptr, 2) == "Teilnahme an 2 Konferenzen");
+}
---
+++ test/core/kernel/test_de.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QPushButton</name>
+    <message>
+       <source>Open</source>
+       <translation>Öffnen</translation>
+    </message>
+    <message>
+       <source>Close</source>
+       <translation>Schließen</translation>
+    </message>
+</context>
+<context>
+    <name>QThing</name>
+    <message numerus="yes">
+       <source>Attending %n conference(s)</source>
+       <translation>
+          <numerusform>Teilnahme an %n Konferenz</numerusform>
+          <numerusform>Teilnahme an %n Konferenzen</numerusform>
+      </translation>
+    </message>
+</context>
+<context>
+    <name>QIODevice</name>
+    <message>
+        <source>Permission denied</source>
+        <translation>Zugriff verweigert</translation>
+    </message>
+    <message>
+        <source>Too many open files</source>
+        <translation>Zu viele Dateien geöffnet</translation>
+    </message>
+</context>
+</TS>
