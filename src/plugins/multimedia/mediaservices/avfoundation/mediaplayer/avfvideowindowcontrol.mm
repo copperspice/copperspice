f6881b8f70051d4e1b785ca24a16f5241a2efab8
Daniel Elliott <danielelliott3d@gmail.com>
2021-03-23 22:40:16 +1300

    add include needed to compiler on macos

--- src/plugins/multimedia/mediaservices/avfoundation/mediaplayer/avfvideowindowcontrol.mm
+++ src/plugins/multimedia/mediaservices/avfoundation/mediaplayer/avfvideowindowcontrol.mm
@@ -24,6 +24,7 @@
 #include "avfvideowindowcontrol.h"
 
 #include <AVFoundation/AVFoundation.h>
+#include <AppKit/NSView.h>
 
 AVFVideoWindowControl::AVFVideoWindowControl(QObject *parent)
    : QVideoWindowControl(parent)
