3d91e7473a00c2fd17eb321b08a8ce69b898b19a
Barbara Geller <barbara@copperspice.com>
2022-01-04 13:10:03 -0800

    Update copyright for CMake

--- cmake/modules/FindIconv.cmake
+++ cmake/modules/FindIconv.cmake
@@ -1,7 +1,7 @@
 # ***********************************************************************
 #
-# Copyright (c) 2012-2021 Barbara Geller
-# Copyright (c) 2012-2021 Ansel Sermersheim
+# Copyright (c) 2012-2022 Barbara Geller
+# Copyright (c) 2012-2022 Ansel Sermersheim
 # Copyright (c) 2015 Ivailo Monev, <xakepa10@gmail.com>
 #
 # This file is part of CopperSpice.
--- cmake/modules/TestLargeFiles.cmake
+++ cmake/modules/TestLargeFiles.cmake
@@ -1,7 +1,7 @@
 # ***********************************************************************
 #
-# Copyright (c) 2012-2021 Barbara Geller
-# Copyright (c) 2012-2021 Ansel Sermersheim
+# Copyright (c) 2012-2022 Barbara Geller
+# Copyright (c) 2012-2022 Ansel Sermersheim
 #
 # This file is part of CopperSpice.
 #
