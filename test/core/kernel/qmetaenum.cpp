1cef135a16d7011788ba930010f49fa719a341dd
Barbara Geller <barbara@copperspice.com>
2022-02-25 16:04:43 -0800

    Add new catch tests for QMetaEnum and QMetaProperty

---
+++ test/core/kernel/qmetaenum.cpp
@@ -0,0 +1,129 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2022 Barbara Geller
+* Copyright (c) 2012-2022 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+// must be first
+#include <qobject.h>
+
+#include <csmeta.h>
+#include <qnamespace.h>
+
+#include <cs_catch2.h>
+
+TEST_CASE("QMetaEnum enum_count_a", "[qmetaenum]")
+{
+   const QMetaObject &metaObject = Qt::staticMetaObject();
+
+   int index = metaObject.indexOfEnumerator("Orientation");
+   QMetaEnum enumObj = metaObject.enumerator(index);
+
+   REQUIRE(enumObj.isValid() == true);
+
+   REQUIRE(enumObj.name()   == "Orientation");
+   REQUIRE(enumObj.scope()  == "Qt");
+   REQUIRE(enumObj.isFlag() == false);
+
+   REQUIRE(enumObj.keyCount() == 2);
+
+   // keys are in a map, in alphabetical order
+   REQUIRE(enumObj.key(0)   == "Horizontal");
+   REQUIRE(enumObj.value(0) == 1);
+
+   REQUIRE(enumObj.key(1)   == "Vertical");
+   REQUIRE(enumObj.value(1) == 2);
+}
+
+TEST_CASE("QMetaEnum enum_count_b", "[qmetaenum]")
+{
+   const QMetaObject &metaObject = Qt::staticMetaObject();
+
+   int index = metaObject.indexOfEnumerator("AlignmentFlag");
+   QMetaEnum enumObj = metaObject.enumerator(index);
+
+   REQUIRE(enumObj.isValid() == true);
+
+   REQUIRE(enumObj.name()   == "AlignmentFlag");
+   REQUIRE(enumObj.scope()  == "Qt");
+   REQUIRE(enumObj.isFlag() == false);
+
+   REQUIRE(enumObj.keyCount() == 14);
+
+   REQUIRE(enumObj.keyToValue("AlignLeft")    == 1);
+   REQUIRE(enumObj.keyToValue("AlignRight")   == 2);
+   REQUIRE(enumObj.keyToValue("AlignVCenter") == 128);
+   REQUIRE(enumObj.keyToValue("AlignHCenter") == 4);
+   REQUIRE(enumObj.keyToValue("AlignTop")     == 32);
+   REQUIRE(enumObj.keyToValue("AlignBottom")  == 64);
+
+   {
+      // part 2
+      int index = metaObject.indexOfEnumerator("Alignment");
+      QMetaEnum enumObj = metaObject.enumerator(index);
+
+      REQUIRE(enumObj.isValid() == true);
+
+      REQUIRE(enumObj.name()   == "Alignment");
+      REQUIRE(enumObj.scope()  == "Qt");
+      REQUIRE(enumObj.isFlag() == true);
+
+      REQUIRE(enumObj.keyToValue("AlignTop")    == 32);
+      REQUIRE(enumObj.keyToValue("AlignBottom") == 64);
+   }
+}
+
+TEST_CASE("QMetaEnum enum_count_c", "[qmetaenum]")
+{
+   const QMetaObject &metaObject = Qt::staticMetaObject();
+
+   int index = metaObject.indexOfEnumerator("SortOrder");
+   QMetaEnum enumObj = metaObject.enumerator(index);
+
+   REQUIRE(enumObj.isValid() == true);
+
+   REQUIRE(enumObj.name()   == "SortOrder");
+   REQUIRE(enumObj.scope()  == "Qt");
+   REQUIRE(enumObj.isFlag() == false);
+
+   // enum values are *not* registered
+   CHECK(enumObj.keyCount() == 0);
+
+   CHECK(enumObj.key(0)   == "");
+   CHECK(enumObj.value(0) == -1);
+}
+
+TEST_CASE("QMetaEnum enum_count_d", "[qmetaenum]")
+{
+   const QMetaObject &metaObject = Qt::staticMetaObject();
+
+   int index = metaObject.indexOfEnumerator("FocusPolicy");
+   QMetaEnum enumObj = metaObject.enumerator(index);
+
+   REQUIRE(enumObj.isValid() == true);
+
+   REQUIRE(enumObj.name()   == "FocusPolicy");
+   REQUIRE(enumObj.scope()  == "Qt");
+   REQUIRE(enumObj.isFlag() == false);
+
+   REQUIRE(enumObj.keyCount() == 5);
+
+   REQUIRE(enumObj.keyToValue("NoFocus")    == 0);
+   REQUIRE(enumObj.keyToValue("TabFocus")   == 1);
+   REQUIRE(enumObj.keyToValue("ClickFocus") == 2);
+}
+
+
---
+++ test/core/kernel/qmetaproperty.cpp
@@ -0,0 +1,126 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2022 Barbara Geller
+* Copyright (c) 2012-2022 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+// must be first
+#include <qobject.h>
+
+#include <qmetaproperty.h>
+#include <qnamespace.h>
+
+#include <cs_catch2.h>
+
+class Ginger_MP : public QObject
+{
+   CS_OBJECT(Ginger_MP)
+
+   CS_PROPERTY_READ(flavor,  getFlavor)
+   CS_PROPERTY_WRITE(flavor, setFlavor)
+
+   CS_PROPERTY_READ(shape,   getCursorShape)
+   CS_PROPERTY_DESIGNABLE(shape, false)
+   CS_PROPERTY_SCRIPTABLE(shape, true)
+   CS_PROPERTY_STORED(shape, false)
+
+   CS_ENUM(Spices)
+
+   public:
+      enum Spices {
+         basil,
+         mint,
+         pepper,
+         thyme,
+      };
+
+      Spices getFlavor() const {
+         return m_flavor;
+      }
+
+      void setFlavor(Spices data) {
+         m_flavor = data;
+      }
+
+      Qt::CursorShape getCursorShape() const {
+         return Qt::BusyCursor;
+      }
+
+   private:
+     Spices m_flavor;
+};
+
+TEST_CASE("QMetaProperty enumerator", "[qmetaproperty]")
+{
+   Ginger_MP obj;
+
+   const QMetaObject &metaObj = Ginger_MP::staticMetaObject();
+
+   int index = metaObj.indexOfProperty("flavor");
+   QMetaProperty prop = metaObj.property(index);
+
+   REQUIRE(prop.isValid() == true);
+
+   QMetaEnum tmpEnum = prop.enumerator();
+
+   REQUIRE(tmpEnum.isValid() == true);
+   REQUIRE(tmpEnum.name() == "Spices");
+
+   REQUIRE(prop.isDesignable() == true);
+}
+
+TEST_CASE("QMetaProperty external_enum", "[qmetaproperty]")
+{
+   Ginger_MP obj;
+
+   const QMetaObject &metaObj = Ginger_MP::staticMetaObject();
+
+   int index = metaObj.indexOfProperty("shape");
+   REQUIRE(index >= 0);
+
+   QMetaProperty prop = metaObj.property(index);
+   REQUIRE(prop.isValid() == true);
+   REQUIRE(prop.name() == "shape");
+
+   QMetaEnum tmpEnum = prop.enumerator();
+
+   REQUIRE(tmpEnum.isValid() == true);
+   REQUIRE(tmpEnum.name() == "CursorShape");
+
+   REQUIRE(prop.isDesignable() == false);
+   REQUIRE(prop.isScriptable() == true);
+   REQUIRE(prop.isStored() == false);
+}
+
+TEST_CASE("QMetaProperty type_name", "[qmetaproperty]")
+{
+   Ginger_MP obj;
+
+   const QMetaObject &metaObj = Ginger_MP::staticMetaObject();
+
+   int index = metaObj.indexOfProperty("flavor");
+   QMetaProperty prop = metaObj.property(index);
+
+   QString name = prop.name();
+
+   REQUIRE(prop.isValid() == true);
+   REQUIRE(prop.isReadable() == true);
+   REQUIRE(prop.isWritable() == true);
+
+   REQUIRE(prop.typeName() == "Ginger_MP::Spices");
+   REQUIRE(name == "flavor");
+}
+
