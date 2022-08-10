40873e56d307a85251494e7750b68115a4e2cd9c
Barbara Geller <barbara@copperspice.com>
2022-08-10 13:46:43 -0700

    Prevent a warning for CMake 3.17 or newer when searching for the XCB package

--- cmake/modules/FindXCB.cmake
+++ cmake/modules/FindXCB.cmake
@@ -29,44 +29,54 @@ set(XCB_INCLUDE_DIRS "")
 set(XCB_LIBRARIES    "")
 
 foreach(comp ${XCB_FIND_COMPONENTS})
-    # component name
-    string(TOUPPER ${comp} compname)
-    string(REPLACE "-" "_" compname ${compname})
+   # component name
+   string(TOUPPER ${comp} compname)
+   string(REPLACE "-" "_" compname ${compname})
 
-    # header name
-    string(REPLACE "xcb-" ""         headername  xcb/${comp}.h)
-    string(REPLACE "-"    ""         headername  ${headername})
-    string(REPLACE "xcb/" "xcb/xcb_" headername2 ${headername})
+   # header name
+   string(REPLACE "xcb-" ""         headername  xcb/${comp}.h)
+   string(REPLACE "-"    ""         headername  ${headername})
+   string(REPLACE "xcb/" "xcb/xcb_" headername2 ${headername})
 
-    # library name
-    set(libname ${comp})
+   # library name
+   set(libname ${comp})
 
-    pkg_check_modules(PC_${comp} QUIET ${comp} xcb)
+   pkg_check_modules(PC_${comp} QUIET ${comp} xcb)
 
-    find_path(${compname}_INCLUDE_DIR NAMES ${headername} ${headername2}
-        HINTS
-        ${PC_${comp}_INCLUDE_DIR}
-        ${PC_${comp}_INCLUDE_DIRS}
-    )
+   find_path(${compname}_INCLUDE_DIR NAMES ${headername} ${headername2}
+      HINTS
+      ${PC_${comp}_INCLUDE_DIR}
+      ${PC_${comp}_INCLUDE_DIRS}
+   )
 
-    find_library(${compname}_LIB NAMES ${libname}
-        HINTS
-        ${PC_${comp}_LIBRARY}
-        ${PC_${comp}_LIBRARY_DIRS}
-    )
+   find_library(${compname}_LIB NAMES ${libname}
+      HINTS
+      ${PC_${comp}_LIBRARY}
+      ${PC_${comp}_LIBRARY_DIRS}
+   )
 
-    find_package_handle_standard_args(${comp}
-        FOUND_VAR ${comp}_FOUND
-        REQUIRED_VARS ${compname}_INCLUDE_DIR  ${compname}_LIB)
+   if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.17.0")
+      find_package_handle_standard_args(${comp}
+         FOUND_VAR ${comp}_FOUND
+         REQUIRED_VARS ${compname}_INCLUDE_DIR  ${compname}_LIB
+         NAME_MISMATCHED
+      )
 
-    mark_as_advanced(${compname}_INCLUDE_DIR   ${compname}_LIB)
+   else()
+      find_package_handle_standard_args(${comp}
+         FOUND_VAR ${comp}_FOUND
+         REQUIRED_VARS ${compname}_INCLUDE_DIR  ${compname}_LIB
+      )
+   endif()
 
-    list(APPEND XCB_INCLUDE_DIR  ${${compname}_INCLUDE_DIR})
-    list(APPEND XCB_LIBRARIES    ${${compname}_LIB})
+   mark_as_advanced(${compname}_INCLUDE_DIR   ${compname}_LIB)
 
-    if(NOT ${comp}_FOUND)
-        set(XCB_FOUND false)
-    endif()
+   list(APPEND XCB_INCLUDE_DIR  ${${compname}_INCLUDE_DIR})
+   list(APPEND XCB_LIBRARIES    ${${compname}_LIB})
+
+   if(NOT ${comp}_FOUND)
+      set(XCB_FOUND false)
+   endif()
 endforeach()
 
 list(REMOVE_DUPLICATES XCB_INCLUDE_DIRS)
