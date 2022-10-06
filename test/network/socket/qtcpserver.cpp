2632e74c4c536f5b89130909024b0141c528c378
Barbara Geller <barbara@copperspice.com>
2022-10-06 11:09:30 -0700

    Add new socket tests

--- test/network/socket/qtcpserver.cpp
+++ test/network/socket/qtcpserver.cpp
@@ -18,6 +18,7 @@
 ***********************************************************************/
 
 #include <qtcpserver.h>
+#include <qtcpsocket.h>
 
 #include <cs_catch2.h>
 
@@ -33,4 +34,270 @@ TEST_CASE("QTcpServer traits", "[qtcpserver]")
    REQUIRE(std::is_nothrow_move_assignable_v<QTcpServer> == false);
 
    REQUIRE(std::has_virtual_destructor_v<QTcpServer> == true);
+}
+
+TEST_CASE("QTcpServer constructor", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   REQUIRE(server.isListening() == false);
+   REQUIRE((int)server.serverPort() == 0);
+   REQUIRE(server.serverAddress() == QHostAddress());
+   REQUIRE(server.maxPendingConnections() == 30);
+   REQUIRE(server.hasPendingConnections() == false);
+   REQUIRE(server.socketDescriptor() == -1);
+   REQUIRE(server.serverError() == QAbstractSocket::UnknownSocketError);
+}
+
+TEST_CASE("QTcpServer clientServerLoop", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   REQUIRE(server.isListening() == false);
+   REQUIRE(server.hasPendingConnections() == false);
+   REQUIRE(server.listen(QHostAddress::Any, 11423) == true);
+   REQUIRE(server.isListening() == true);
+
+   QTcpSocket client;
+   QHostAddress serverAddress = QHostAddress::LocalHost;
+
+   if (! (server.serverAddress() == QHostAddress::Any) && ! (server.serverAddress() == QHostAddress::AnyIPv6) &&
+         ! (server.serverAddress() == QHostAddress::AnyIPv4)) {
+       serverAddress = server.serverAddress();
+   }
+
+   client.connectToHost(serverAddress, server.serverPort());
+   REQUIRE(client.waitForConnected(5000));
+
+   REQUIRE(server.waitForNewConnection(5000));
+   REQUIRE(server.hasPendingConnections());
+
+   QTcpSocket *socket = server.nextPendingConnection();
+   REQUIRE(socket != nullptr);
+
+   REQUIRE(socket->write("Hello client\n", 13) == 13);
+   socket->flush();
+
+   REQUIRE(client.waitForReadyRead(5000));
+
+   QByteArray data = client.readAll();
+   REQUIRE(data == "Hello client\n");
+
+   REQUIRE(client.write("Nice to see you Server\n", 23) == 23);
+   client.flush();
+
+   REQUIRE(socket->waitForReadyRead(5000));
+
+   data = socket->readAll();
+   REQUIRE(data == "Nice to see you Server\n");
+}
+
+TEST_CASE("QTcpServer dualStack", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QHostAddress bindAddress;
+
+   printf("Network Dual Stack (test 1 of 3)\n");
+
+   {
+      QTcpServer server;
+      QTcpSocket v4client;
+      QTcpSocket v6client;
+
+      bindAddress = QHostAddress::Any;
+
+      REQUIRE(server.listen(bindAddress));
+
+      v4client.connectToHost(QHostAddress::LocalHost,     server.serverPort());
+      v6client.connectToHost(QHostAddress::LocalHostIPv6, server.serverPort());
+
+      REQUIRE(v4client.waitForConnected(3000) == true);
+      REQUIRE(v6client.waitForConnected(3000) == true);
+   }
+
+   printf("Network Dual Stack (test 2 of 3)\n");
+
+   {
+      QTcpServer server;
+      QTcpSocket v4client;
+      QTcpSocket v6client;
+
+      bindAddress = QHostAddress::AnyIPv4;
+
+      REQUIRE(server.listen(bindAddress));
+
+      v4client.connectToHost(QHostAddress::LocalHost,     server.serverPort());
+      v6client.connectToHost(QHostAddress::LocalHostIPv6, server.serverPort());
+
+      REQUIRE(v4client.waitForConnected(3000) == true);
+      REQUIRE(v6client.waitForConnected(3000) == false);
+   }
+
+   printf("Network Dual Stack (test 3 of 3)\n");
+
+   {
+      QTcpServer server;
+      QTcpSocket v4client;
+      QTcpSocket v6client;
+
+      bindAddress = QHostAddress::AnyIPv6;
+
+      REQUIRE(server.listen(bindAddress));
+
+      v4client.connectToHost(QHostAddress::LocalHost,     server.serverPort());
+      v6client.connectToHost(QHostAddress::LocalHostIPv6, server.serverPort());
+
+      REQUIRE(v4client.waitForConnected(3000) == false);
+      REQUIRE(v6client.waitForConnected(3000) == true);
+   }
+}
+
+TEST_CASE("QTcpServer ipv6Server", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   if (! server.listen(QHostAddress::LocalHostIPv6, 8944)) {
+      REQUIRE(server.serverError() == QAbstractSocket::UnsupportedSocketOperationError);
+
+   } else {
+      REQUIRE(server.serverPort() == quint16(8944));
+      REQUIRE(server.serverAddress() == QHostAddress::LocalHostIPv6);
+
+      QTcpSocket client;
+      client.connectToHost("::1", 8944);
+
+      REQUIRE(client.waitForConnected(5000)  == true);
+      REQUIRE(server.waitForNewConnection()  == true);
+      REQUIRE(server.hasPendingConnections() == true);
+
+      QTcpSocket *serverSocket = nullptr;
+
+      REQUIRE((serverSocket = server.nextPendingConnection()));
+
+      serverSocket->close();
+      delete serverSocket;
+   }
+}
+
+TEST_CASE("QTcpServer ipv6ServerMapped", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   REQUIRE(server.listen(QHostAddress::LocalHost) == true);
+
+   printf("Network Server v6 Mapped (test 1 of 4)\n");
+
+   {
+      QTcpSocket client;
+      client.connectToHost("127.0.0.1", server.serverPort());
+
+      REQUIRE(server.waitForNewConnection(3000) == true);
+      delete server.nextPendingConnection();
+   }
+
+   printf("Network Server v6 Mapped (test 2 of 4)\n");
+
+   {
+      QTcpSocket client;
+      client.connectToHost("::ffff:127.0.0.1", server.serverPort());
+
+      REQUIRE(server.waitForNewConnection(3000) == true);
+      delete server.nextPendingConnection();
+   }
+
+   printf("Network Server v6 Mapped (test 3 of 4)\n");
+
+   {
+      QTcpSocket client;
+      client.connectToHost("::ffff:7F00:0001", server.serverPort());
+
+      REQUIRE(server.waitForNewConnection(5000) == true);
+      delete server.nextPendingConnection();
+   }
+
+   printf("Network Server v6 Mapped (test 4 of 4)\n");
+
+   {
+      QTcpSocket client;
+      client.connectToHost("::1", server.serverPort());
+
+      REQUIRE(server.waitForNewConnection(5000) == false);
+   }
+}
+
+void emptyHandler(QtMsgType, QStringView)
+{
+}
+
+TEST_CASE("QTcpServer listen", "[qtcpserver]")
+{
+   QTcpServer server;
+
+   auto oldHandler = csInstallMsgHandler(emptyHandler);
+
+   REQUIRE(server.listen() == true);
+   REQUIRE(server.listen() == false);
+
+   csInstallMsgHandler(oldHandler);
+}
+
+TEST_CASE("QTcpServer maxPendingConnections", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   QTcpSocket socket1;
+   QTcpSocket socket2;
+   QTcpSocket socket3;
+
+   server.setMaxPendingConnections(2);
+   REQUIRE(server.listen() == true);
+
+   socket1.connectToHost(QHostAddress::LocalHost, server.serverPort());
+   socket2.connectToHost(QHostAddress::LocalHost, server.serverPort());
+   socket3.connectToHost(QHostAddress::LocalHost, server.serverPort());
+
+   printf("Network Max Pending Connections\n");
+
+   REQUIRE(server.waitForNewConnection(3000));
+
+   REQUIRE(server.hasPendingConnections()  == true);
+   REQUIRE(server.nextPendingConnection()  != nullptr);
+
+   REQUIRE(server.hasPendingConnections()  == true);
+   REQUIRE(server.nextPendingConnection()  != nullptr);
+
+   REQUIRE(server.hasPendingConnections()  == false);
+   REQUIRE(server.nextPendingConnection()  == nullptr);
+
+   REQUIRE(server.waitForNewConnection(3000) == true);
+
+   REQUIRE(server.hasPendingConnections() == true);
+   REQUIRE(server.nextPendingConnection() != nullptr);
+}
+
+TEST_CASE("QTcpServer setMax", "[qtcpserver]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   server.setMaxPendingConnections(0);
+   REQUIRE(server.maxPendingConnections() == 0);
+
+   server.setMaxPendingConnections(INT_MIN);
+   REQUIRE(server.maxPendingConnections() == INT_MIN);
+
+   server.setMaxPendingConnections(INT_MAX);
+   REQUIRE(server.maxPendingConnections() == INT_MAX);
 }
--- test/network/socket/qtcpsocket.cpp
+++ test/network/socket/qtcpsocket.cpp
@@ -17,6 +17,8 @@
 *
 ***********************************************************************/
 
+#include <qsslsocket.h>
+#include <qtcpserver.h>
 #include <qtcpsocket.h>
 
 #include <cs_catch2.h>
@@ -33,4 +35,116 @@ TEST_CASE("QTcpSocket traits", "[qtcpsocket]")
    REQUIRE(std::is_nothrow_move_assignable_v<QTcpSocket> == false);
 
    REQUIRE(std::has_virtual_destructor_v<QTcpSocket> == true);
+}
+
+static QTcpSocket * newSocket()
+{
+   QTcpSocket *socket;
+
+#ifdef QT_SSL
+   // path to openSSL library required
+   socket = new QSslSocket;
+#else
+   socket = new QTcpSocket;
+#endif
+
+   return socket;
+}
+
+TEST_CASE("QTcpSocket constructor", "[qtcpsocket]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpSocket *socket = newSocket();
+
+   REQUIRE(socket->state()        == QTcpSocket::UnconnectedState);
+   REQUIRE(socket->isSequential() == true);
+   REQUIRE(socket->isOpen()       == false);
+   REQUIRE(socket->isValid()      == false);
+   REQUIRE(socket->socketType()   == QTcpSocket::TcpSocket);
+
+   char c;
+
+   REQUIRE(socket->getChar(&c)        == false);
+   REQUIRE(socket->bytesAvailable()   == 0);
+   REQUIRE(socket->canReadLine()      == false);
+   REQUIRE(socket->readLine()         == QByteArray());
+   REQUIRE(socket->socketDescriptor() == (qintptr)-1);
+   REQUIRE(socket->localPort()        == 0);
+   REQUIRE(socket->localAddress()     == QHostAddress());
+   REQUIRE(socket->peerPort()         == 0);
+   REQUIRE(socket->peerAddress()      == QHostAddress());
+   REQUIRE(socket->error()            == QTcpSocket::UnknownSocketError);
+   REQUIRE(socket->errorString()      == QString("Unknown error"));
+
+   delete socket;
+}
+
+TEST_CASE("QTcpSocket bindThenResolveHost", "[qtcpsocket]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpServer server;
+
+   QHostAddress serverAddress = QHostAddress::LocalHost;
+   const quint16 port = 11423;
+
+   REQUIRE(server.listen(QHostAddress::Any, port) == true);
+   REQUIRE(server.isListening() == true);
+
+   // part one
+   QTcpSocket *socket = newSocket();
+
+   REQUIRE(socket->bind(QHostAddress(QHostAddress::AnyIPv4), 0) == true);
+   REQUIRE(socket->state() == QAbstractSocket::BoundState);
+
+   quint16 boundPort = socket->localPort();
+   qintptr fd = socket->socketDescriptor();
+
+   REQUIRE(fd != -1);
+
+   // part two
+   socket->connectToHost(serverAddress, port);
+
+   REQUIRE((socket->state() == QAbstractSocket::HostLookupState
+         || socket->state() == QAbstractSocket::ConnectingState));
+
+   REQUIRE(socket->waitForConnected(3000) == true);
+
+   REQUIRE(socket->state() == QAbstractSocket::ConnectedState);
+
+   REQUIRE(socket->localPort() == boundPort);
+   REQUIRE(socket->socketDescriptor() == fd);
+
+   delete socket;
+}
+
+TEST_CASE("QTcpSocket descripter", "[qtcpsocket]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpSocket *socket = newSocket();
+
+   REQUIRE(socket->socketDescriptor() == (qintptr)-1);
+
+   REQUIRE(socket->setSocketDescriptor(-5, QTcpSocket::UnconnectedState) == false);
+   REQUIRE(socket->socketDescriptor() == (qintptr)-1);
+
+   REQUIRE(socket->error() == QTcpSocket::UnsupportedSocketOperationError);
+
+   delete socket;
+}
+
+TEST_CASE("QTcpSocket connectToHost", "[qtcpsocket]")
+{
+   auto testApp = initCoreApp();
+
+   QTcpSocket *socket = newSocket();
+   socket->connectToHost("nosuchserver.copperspice.com", 80);
+
+   REQUIRE(socket->waitForConnected() == false);
+   REQUIRE(socket->state() == QTcpSocket::UnconnectedState);
+   REQUIRE(socket->error() == QTcpSocket::HostNotFoundError);
+
+   delete socket;
 }
