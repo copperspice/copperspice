bca271481743d64bdb11faf98a1ebe04155fe163
Ansel Sermersheim <ansel@copperspice.com>
2022-12-16 13:26:52 -0800

    Add functionality when calling find_package(CopperSpice) from a downstream project
    - test occurs for MSVC or when the downstream CMake variable
      CS_CHECK_BUILD_TYPE is true
    
    - build type of downstream project must match the build of CS, otherwise
      the find package fails

--- cmake/CopperSpiceConfigVersion.cmake
+++ cmake/CopperSpiceConfigVersion.cmake
@@ -24,6 +24,39 @@ set(COPPERSPICE_VERSION_API   "@BUILD_MAJOR@.@BUILD_MINOR@" PARENT_SCOPE)
 
 set(PACKAGE_VERSION "@BUILD_MAJOR@.@BUILD_MINOR@.@BUILD_MICRO@")
 
+if (MSVC OR "${CS_CHECK_BUILD_TYPE}")
+   # used when a downstream project calls find_package() for cs
+   set(PACKAGE_VERSION_COMPATIBLE false)
+
+   # first test the down stream project, then test cs
+   set(Debug_Types "Debug" "")
+
+   if ("${CMAKE_BUILD_TYPE}" IN_LIST Debug_Types)
+
+      if ("@CMAKE_BUILD_TYPE@" IN_LIST Debug_Types)
+         set(PACKAGE_VERSION_COMPATIBLE true)
+      endif()
+
+   endif()
+
+   # first test the down stream project, then test cs
+   set(Release_Types "Release" "MinSizeRel" "RelWithDebInfo")
+
+   if ("${CMAKE_BUILD_TYPE}" IN_LIST Release_Types)
+
+      if("@CMAKE_BUILD_TYPE@" IN_LIST Release_Types)
+         set(PACKAGE_VERSION_COMPATIBLE true)
+      endif()
+
+   endif()
+
+   if(NOT ${PACKAGE_VERSION_COMPATIBLE})
+      # found a copy of cs with a mismatch build type, marked as not found
+      set(PACKAGE_VERSION_UNSUITABLE true)
+   endif()
+
+endif()
+
 # used when a downstream project calls find_package() for cs with a version number
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
    set(PACKAGE_VERSION_COMPATIBLE FALSE)
