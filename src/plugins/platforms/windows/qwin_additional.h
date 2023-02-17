0207dace0ce0078291b1d8314d7b905acd091424
Barbara Geller <barbara@copperspice.com>
2023-02-17 08:47:16 -0800

    Remove struct declaration which is now defined in the Windows API

--- src/plugins/platforms/windows/qwin_additional.h
+++ src/plugins/platforms/windows/qwin_additional.h
@@ -121,39 +121,9 @@ typedef struct tagUPDATELAYEREDWINDOWINFO {
 
 #endif // if defined(Q_CC_MINGW)
 
-/* Touch is supported from Windows 7 onwards and data structures
- * are present in the Windows SDK's, but not in older MSVC Express
- * versions. */
-
-#if defined(Q_CC_MINGW) || !defined(TOUCHEVENTF_MOVE)
-
-#define WM_TOUCH 0x0240
-
-typedef struct tagTOUCHINPUT {
-   LONG x;
-   LONG y;
-   HANDLE hSource;
-   DWORD dwID;
-   DWORD dwFlags;
-   DWORD dwMask;
-   DWORD dwTime;
-   ULONG_PTR dwExtraInfo;
-   DWORD cxContact;
-   DWORD cyContact;
-} TOUCHINPUT, *PTOUCHINPUT;
-typedef TOUCHINPUT const *PCTOUCHINPUT;
-
-#    define TOUCHEVENTF_MOVE 0x0001
-#    define TOUCHEVENTF_DOWN 0x0002
-#    define TOUCHEVENTF_UP 0x0004
-#    define TOUCHEVENTF_INRANGE 0x0008
-#    define TOUCHEVENTF_PRIMARY 0x0010
-#    define TOUCHEVENTF_NOCOALESCE 0x0020
-#    define TOUCHEVENTF_PALM 0x0080
-#    define TOUCHINPUTMASKF_CONTACTAREA 0x0004
-#    define TOUCHINPUTMASKF_EXTRAINFO 0x0002
-
-#endif // if defined(Q_CC_MINGW) || !defined(TOUCHEVENTF_MOVE)
+#ifndef WM_TOUCH
+#   define WM_TOUCH 0x0240
+#endif
 
 #ifndef WM_GESTURE
 #  define WM_GESTURE 0x0119
