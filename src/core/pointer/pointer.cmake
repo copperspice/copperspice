a3616efb5be3e83d6ac8ad9447a09c46717ff1bc
Barbara Geller <barbara@copperspice.com>
2023-02-23 09:18:04 -0800

    Update CMake build files to use the new pointer files

--- src/core/pointer/pointer.cmake
+++ src/core/pointer/pointer.cmake
@@ -3,9 +3,6 @@ list(APPEND CORE_PUBLIC_INCLUDES
    QPointer
    QScopedArrayPointer
    QScopedPointer
-   QScopedPointerArrayDeleter
-   QScopedPointerDeleter
-   QScopedPointerPodDeleter
    QScopedValueRollback
    QSharedData
    QSharedDataPointer
@@ -13,28 +10,42 @@ list(APPEND CORE_PUBLIC_INCLUDES
    QWeakPointer
 )
 
+if (false)
+   # use system headers
+
+else()
+   # use annex headers
+   target_include_directories(CsCore
+      PUBLIC
+      $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/annex/cs_pointer>
+   )
+
+   list(APPEND CORE_INCLUDES
+      ${CMAKE_CURRENT_SOURCE_DIR}/../annex/cs_pointer/cs_unique_pointer.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/../annex/cs_pointer/cs_unique_array_pointer.h
+   )
+endif()
+
 list(APPEND CORE_INCLUDES
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qexplicitlyshareddatapointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qpointer.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedarraypointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedpointer.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedpointerarraydeleter.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedpointerdeleter.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedpointerpoddeleter.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedarraypointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedvaluerollback.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qshareddata.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qshareddatapointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qsharedpointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qsharedpointer_impl.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/quniquepointer.h
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qweakpointer.h
 )
 
 target_sources(CsCore
    PRIVATE
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qpointer.cpp
-   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedpointer.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qscopedvaluerollback.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qshareddata.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/pointer/qsharedpointer.cpp
+   ${CMAKE_CURRENT_SOURCE_DIR}/pointer/quniquepointer.cpp
 )
 
