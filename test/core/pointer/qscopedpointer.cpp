f692570b8a9ebd6b9572aa3b29e389b60169e011
Ansel Sermersheim <ansel@copperspice.com>
2023-02-24 13:47:30 -0800

    Add new catch tests for QScopedPointer and QScopedArrayPointer

--- test/core/pointer/qscopedpointer.cpp
+++ test/core/pointer/qscopedpointer.cpp
@@ -32,3 +32,126 @@ TEST_CASE("QScopedPointer traits", "[qscopedpointer]")
 
    REQUIRE(std::has_virtual_destructor_v<QScopedPointer<int>> == false);
 }
+
+TEST_CASE("QScopedPointer convert", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr1 = QMakeScoped<int>(42);
+
+   std::unique_ptr<int> ptr2 = std::move(ptr1);
+
+   REQUIRE(ptr1 == nullptr);
+   REQUIRE(ptr2 != nullptr);
+
+   REQUIRE(*ptr2 == 42);
+}
+
+TEST_CASE("QScopedPointer custom_deleter", "[qscopedpointer]")
+{
+   bool deleterExecuted = false;
+
+   {
+      auto customDeleter = [&deleterExecuted] (int *obj) {
+         deleterExecuted = true;
+         delete obj;
+      };
+
+      QScopedPointer<int, decltype(customDeleter)> ptr(new int, customDeleter);
+      REQUIRE(deleterExecuted == false);
+   }
+
+   REQUIRE(deleterExecuted == true);
+}
+
+TEST_CASE("QScopedPointer empty", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr;
+
+   REQUIRE(ptr == nullptr);
+   REQUIRE(nullptr == ptr);
+   REQUIRE(ptr == ptr);
+
+   REQUIRE(! (ptr != nullptr));
+   REQUIRE(! (nullptr != ptr));
+   REQUIRE(! (ptr != ptr)) ;
+
+   REQUIRE(ptr.isNull() == true);
+}
+
+TEST_CASE("QScopedPointer move_assign", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr1;
+   int *rawPointer = nullptr;
+
+   {
+      QScopedPointer<int> ptr2(new int);
+      rawPointer = ptr2.data();
+      ptr1 = std::move(ptr2);
+
+      REQUIRE(ptr2.isNull());
+   }
+
+   REQUIRE(rawPointer == ptr1.get());
+}
+
+TEST_CASE("QScopedPointer move_construct", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr1 = QMakeUnique<int>();
+   QScopedPointer<int> ptr2(std::move(ptr1));
+
+   REQUIRE(ptr1.isNull() == true);
+   REQUIRE(ptr2.isNull() == false);
+}
+
+TEST_CASE("QScopedPointer release", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr = QMakeScoped<int>();
+   int *p1 = ptr.get();
+   int *p2 = ptr.release();
+
+   REQUIRE(p1 == p2);
+   REQUIRE(ptr == nullptr);
+
+   delete p2;
+
+   REQUIRE(ptr.release() == nullptr);
+}
+
+TEST_CASE("QScopedPointer reset", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr = QMakeUnique<int>();
+   ptr.reset();
+
+   REQUIRE(ptr == nullptr);
+   REQUIRE(ptr.isNull() == true);
+}
+
+TEST_CASE("QScopedPointer swap", "[qscopedpointer]")
+{
+   QScopedPointer<int> ptr1 = QMakeUnique<int>(8);
+   QScopedPointer<int> ptr2 = QMakeUnique<int>(17);
+
+   REQUIRE(*ptr1 == 8);
+   REQUIRE(*ptr2 == 17);
+
+   ptr1.swap(ptr2);
+
+   REQUIRE(*ptr1 == 17);
+   REQUIRE(*ptr2 == 8);
+
+   ptr1.reset();
+   ptr1.swap(ptr2);
+
+   REQUIRE(*ptr1 == 8);
+   REQUIRE(ptr2 == nullptr);
+
+   ptr1.swap(ptr1);
+
+   REQUIRE(*ptr1 == 8);
+   REQUIRE(ptr2 == nullptr);
+
+   ptr2.swap(ptr2);
+
+   REQUIRE(*ptr1 == 8);
+   REQUIRE(ptr2 == nullptr);
+}
+
