606937c8f90c4b6a83930aba7cd1a3eab365c8b4
Barbara Geller <barbara@copperspice.com>
2023-08-24 13:24:27 -0700

    Add main source for GUI Catch tests

---
+++ test/gui/test_main.cpp
@@ -0,0 +1,37 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2023 Barbara Geller
+* Copyright (c) 2012-2023 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include <qvariantgui_p.h>
+
+void registerGuiVariant()
+{
+   static bool isRegistered = false;
+
+   if (! isRegistered) {
+      // add gui to the variant system
+      static QVariantGui objVariant;
+      QVariant::registerClient(&objVariant);
+
+      isRegistered = true;
+   }
+}
