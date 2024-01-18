070471c503f2facda1343a12e361666d063beeb5
Barbara Geller <barbara@copperspice.com>
2024-01-18 14:51:25 -0800

    Update copyright for catch tests

--- test/network/kernel/qhostinfo.cpp
+++ test/network/kernel/qhostinfo.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2023 Barbara Geller
-* Copyright (c) 2012-2023 Ansel Sermersheim
+* Copyright (c) 2012-2024 Barbara Geller
+* Copyright (c) 2012-2024 Ansel Sermersheim
 *
 * This file is part of CopperSpice.
 *
