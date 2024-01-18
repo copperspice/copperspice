abcabae5b973ff58402c8a91c7b5f8b5eb621d6a
Barbara Geller <barbara@copperspice.com>
2024-01-18 14:10:21 -0800

    Update copyright for various other libraries

--- src/dbus/qdbuscontext.h
+++ src/dbus/qdbuscontext.h
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2022 Barbara Geller
-* Copyright (c) 2012-2022 Ansel Sermersheim
+* Copyright (c) 2012-2024 Barbara Geller
+* Copyright (c) 2012-2024 Ansel Sermersheim
 *
 * Copyright (c) 2015 The Qt Company Ltd.
 * Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
