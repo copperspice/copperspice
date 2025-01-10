28211b7b0a9945dcd3d21d9902f9b135406d8be0
Barbara Geller <barbara@copperspice.com>
2025-01-10 10:05:38 -0800

    Update copyright for CsCore

--- src/core/global/qnumeric_p.h
+++ src/core/global/qnumeric_p.h
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2024 Barbara Geller
-* Copyright (c) 2012-2024 Ansel Sermersheim
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
 *
 * Copyright (c) 2015 The Qt Company Ltd.
 * Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
