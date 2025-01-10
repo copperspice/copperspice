38b90f68ffaaa37a52e517f71f3dd4a1500e4c21
Barbara Geller <barbara@copperspice.com>
2025-01-10 08:51:26 -0800

    Update copyright for 3rd Party

--- src/3rdparty/sha1/sha1.cpp
+++ src/3rdparty/sha1/sha1.cpp
@@ -1,9 +1,9 @@
 /***********************************************************************
 *
-* Copyright (c) 2012-2024 Barbara Geller
-* Copyright (c) 2012-2024 Ansel Sermersheim
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
 *
-* Copyright (C) 2015 The Qt Company Ltd.
+* Copyright (c) 2015 The Qt Company Ltd.
 * Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
 * Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
 *
