592bc413b76650008e18614c1f13342cf75a73e8
Barbara Geller <barbara@copperspice.com>
2025-02-06 09:33:06 -0800

    Add new unit test file

---
+++ test/gui/text/qtextobject.cpp
@@ -0,0 +1,33 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#include <qtextobject.h>
+
+#include <cs_catch2.h>
+
+TEST_CASE("QTextBlock::iterator traits", "[qtextblock::iterator]")
+{
+   REQUIRE(std::is_copy_constructible_v<QTextBlock::iterator> == true);
+   REQUIRE(std::is_move_constructible_v<QTextBlock::iterator> == true);
+
+   REQUIRE(std::is_copy_assignable_v<QTextBlock::iterator> == true);
+   REQUIRE(std::is_move_assignable_v<QTextBlock::iterator> == true);
+
+   REQUIRE(std::has_virtual_destructor_v<QTextBlock::iterator> == false);
+}
