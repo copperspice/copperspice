4b5e14654175b81d088dfe365fc185afbd60999b
Ansel Sermersheim <ansel@copperspice.com>
2025-04-21 12:04:40 -0700

    Improve and add new unit tests for QSet and QStack

--- test/core/containers/qset.cpp
+++ test/core/containers/qset.cpp
@@ -32,6 +32,19 @@ TEST_CASE("QSet traits", "[qset]")
    REQUIRE(std::has_virtual_destructor_v<QSet<int>> == false);
 }
 
+TEST_CASE("QSet begin_end", "[qset]")
+{
+   QSet<QString> setA = { "watermelon", "apple", "pear", "grapefruit" };
+   QSet<QString> setB;
+
+   for (auto iter = setA.begin(); iter != setA.end(); ++iter)  {
+      setB.insert(*iter);
+   }
+
+   REQUIRE(setA.size() == setB.size());
+   REQUIRE(setA == setB);
+}
+
 TEST_CASE("QSet clear", "[qset]")
 {
    QSet<QString> set = { "watermelon", "apple", "pear", "grapefruit" };
@@ -45,8 +58,8 @@ TEST_CASE("QSet contains_a", "[qset]")
 {
    QSet<QString> set = { "watermelon", "apple", "pear", "grapefruit" };
 
-   REQUIRE(set.contains("pear"));
-   REQUIRE(! set.contains("mango"));
+   REQUIRE(set.contains("pear")  == true);
+   REQUIRE(set.contains("mango") == false);
 }
 
 TEST_CASE("QSet contains_b", "[qset]")
@@ -55,7 +68,7 @@ TEST_CASE("QSet contains_b", "[qset]")
    QSet<QString> set2 = { "grape", "orange", "apple" };
 
    REQUIRE(set1.contains(set1) == true);
-   REQUIRE(set1.contains(set2) == false);      // FAILS
+   REQUIRE(set1.contains(set2) == false);
 
    set1.insert("orange");
    set1.insert("grape");
@@ -63,14 +76,28 @@ TEST_CASE("QSet contains_b", "[qset]")
    REQUIRE(set1.contains(set2) == true);
 }
 
+TEST_CASE("QSet duplicate", "[qset]")
+{
+   QSet<QString> set = { "watermelon", "apple", "pear", "grapefruit" };
+
+   REQUIRE(set.contains("apple"));
+   REQUIRE(set.size() == 4);
+
+   set.insert("apple");
+
+   REQUIRE(set.contains("apple"));
+   REQUIRE(set.size() == 4);
+}
+
 TEST_CASE("QSet empty", "[qset]")
 {
    QSet<QString> set;
 
-   REQUIRE(set.isEmpty());
+   REQUIRE(set.isEmpty() == true);
+   REQUIRE(set.size() == 0);
 }
 
-TEST_CASE("QSet equality", "[qset]")
+TEST_CASE("QSet comparison", "[qset]")
 {
    QSet<QString> set1 = { "watermelon", "apple", "pear", "grapefruit" };
    QSet<QString> set2 = { "watermelon", "apple", "pear", "grapefruit" };
@@ -93,6 +120,80 @@ TEST_CASE("QSet erase", "[qset]")
    REQUIRE(set.contains("grapefruit"));
 
    REQUIRE(set.size() == 3);
+}
+
+TEST_CASE("QSet intersect", "[qset]")
+{
+   QSet<QString> setA;
+   setA << "apple" << "banana" << "cherry";
+
+   QSet<QString> setB;
+   setB << "banana" << "cherry" << "date";
+
+   {
+      QSet<QString> result = setA.intersect(setB);
+
+      REQUIRE(result.size() == 2);
+      REQUIRE(result.contains("banana"));
+      REQUIRE(result.contains("cherry"));
+
+      REQUIRE(result.contains("apple") == false);
+      REQUIRE(result.contains("date")  == false);
+
+      REQUIRE(result == setA);
+
+      REQUIRE(setA.size() == 2);
+      REQUIRE(setB.size() == 3);
+   }
+
+   {
+      QSet<QString> setC;
+      setC << "kiwi" << "mango";
+
+      QSet<QString> result = setA.intersect(setC);
+
+      REQUIRE(setA.size() == 0);
+
+      REQUIRE(result.size() == 0);
+      REQUIRE(result.isEmpty() == true);
+   }
+
+   {
+      // reset values
+      setA << "apple" << "banana" << "cherry";
+
+      QSet<QString> result = setA.intersect(QSet<QString>());
+
+      REQUIRE(setA.size() == 0);
+
+      REQUIRE(result.size() == 0);
+      REQUIRE(result.isEmpty() == true);
+   }
+
+   {
+      // reset values
+      setA << "apple" << "banana" << "cherry";
+
+      QSet<QString> setD = setA;
+
+      REQUIRE(setA.size() == 3);
+      REQUIRE(setD.size() == 3);
+
+      REQUIRE(setD.contains("apple")  == true);
+      REQUIRE(setD.contains("banana") == true);
+      REQUIRE(setD.contains("cherry") == true);
+   }
+
+   {
+      QSet<QString> setE;
+      setE << "apple" << "date" << "cherry";
+
+      setE &= setB;
+
+      REQUIRE(setE.size() == 2);
+      REQUIRE(setE.contains("date"));
+      REQUIRE(setE.contains("cherry"));
+   }
 }
 
 TEST_CASE("QSet insert", "[qset]")
@@ -138,3 +239,66 @@ TEST_CASE("QSet swap", "[qset]")
    REQUIRE(! set2.contains("orange"));
 }
 
+TEST_CASE("QSet toList", "[qset]")
+{
+    QSet<QString> set = { "watermelon", "apple", "pear", "grapefruit" };
+
+    SECTION("Convert to QList") {
+        QList<QString> list = set.toList();
+
+        REQUIRE(list.size() == 4);
+
+        REQUIRE(list.contains("watermelon"));
+        REQUIRE(list.contains("apple"));
+        REQUIRE(list.contains("pear"));
+        REQUIRE(list.contains("grapefruit"));
+    }
+
+    SECTION("Modify QList") {
+        QList<QString> list = set.toList();
+        list.removeAll("pear");
+
+        REQUIRE(list.size() == 3);
+        REQUIRE(list.contains("apple") == true);
+        REQUIRE(list.contains("pear") == false);
+
+        REQUIRE(set.size() == 4);
+        REQUIRE(set.contains("pear") == true);
+    }
+}
+
+TEST_CASE("QSet unite", "[qset]")
+{
+   QSet<QString> setA = { "watermelon", "apple", "pear", "grapefruit" };
+   QSet<QString> setB = { "grape", "orange", "pear"};
+
+   {
+      QSet<QString> result = setA.unite(setB);
+
+      REQUIRE(result.size() == 6);
+      REQUIRE(result.contains("watermelon") == true);
+      REQUIRE(result.contains("apple") == true);
+      REQUIRE(result.contains("pear") == true);
+      REQUIRE(result.contains("grapefruit") == true);
+
+      REQUIRE(result.contains("grape") == true);
+      REQUIRE(result.contains("orange") == true);
+   }
+
+   {
+      QSet<QString> result = setA.unite(QSet<QString>());
+
+      REQUIRE(result == setA);
+   }
+
+   {
+      QSet<QString> setC;
+      setC << "peach" << "apple";
+
+      QSet<QString> result = setA.unite(setB).unite(setC);
+
+      REQUIRE(result.size() == 7);
+      REQUIRE(result.contains("peach"));
+      REQUIRE(result.contains("apple"));
+   }
+}
--- test/core/containers/qstack.cpp
+++ test/core/containers/qstack.cpp
@@ -32,11 +32,54 @@ TEST_CASE("QStack traits", "[qstack]")
    REQUIRE(std::has_virtual_destructor_v<QStack<int>> == false);
 }
 
+TEST_CASE("QStack begin_end", "[qstack]")
+{
+   QStack<QString> stack;
+   stack.push("watermelon");
+   stack.push("apple");
+   stack.push("pear");
+   stack.push("grapefruit");
+
+   {
+      auto iterBegin = stack.begin();
+      auto iterEnd   = stack.end();
+
+      REQUIRE(*iterBegin == "watermelon");
+      REQUIRE(*(iterEnd - 1) == "grapefruit");
+   }
+
+   {
+      auto iterBegin = stack.constBegin();
+      auto iterEnd   = stack.constEnd();
+
+      REQUIRE(*iterBegin == "watermelon");
+      REQUIRE(*(iterEnd - 1) == "grapefruit");
+   }
+
+   {
+      auto iterBegin = stack.cbegin();
+      auto iterEnd   = stack.cend();
+
+      REQUIRE(*iterBegin == "watermelon");
+      REQUIRE(*(iterEnd - 1) == "grapefruit");
+   }
+
+   {
+      QStack<QString>::const_iterator iter = stack.begin();
+
+      REQUIRE(iter == stack.cbegin());
+      REQUIRE(iter != stack.cend());
+
+      REQUIRE(iter == stack.begin());
+   }
+}
+
 TEST_CASE("QStack empty", "[qstack]")
 {
    QStack<QString> stack;
 
    REQUIRE(stack.isEmpty());
+   REQUIRE(stack.size() == 0);
 }
 
 TEST_CASE("QStack length", "[qstack]")
@@ -139,6 +182,22 @@ TEST_CASE("QStack insert", "[qstack]")
    REQUIRE(stack.length() == 5);
 }
 
+TEST_CASE("QStack pop", "[qstack]")
+{
+   QStack<QString> stack;
+   stack.push("watermelon");
+   stack.push("apple");
+   stack.push("pear");
+   stack.push("grapefruit");
+
+   REQUIRE(stack.pop() == "grapefruit");
+   REQUIRE(stack.pop() == "pear");
+   REQUIRE(stack.size() == 2);
+
+   REQUIRE(stack.contains("pear") == false);
+   REQUIRE(stack.contains("apple") == true);
+}
+
 TEST_CASE("QStack position", "[qstack]")
 {
    QStack<QString> stack;
@@ -147,9 +206,62 @@ TEST_CASE("QStack position", "[qstack]")
    stack.push("pear");
    stack.push("grapefruit");
 
+   REQUIRE(stack.top()   == "grapefruit");
+
    REQUIRE(stack.first() == "watermelon");
    REQUIRE(stack.last()  == "grapefruit");
 
    REQUIRE(stack.front() == "watermelon");
    REQUIRE(stack.back()  == "grapefruit");
+}
+
+TEST_CASE("QStack toList", "[qstack]")
+{
+   QStack<QString> stack;
+   stack.push("watermelon");
+   stack.push("apple");
+   stack.push("pear");
+   stack.push("grapefruit");
+
+   SECTION("Convert to QList") {
+        QList<QString> list = stack.toList();
+
+        REQUIRE(list.size() == 4);
+
+        REQUIRE(list.contains("watermelon"));
+        REQUIRE(list.contains("apple"));
+        REQUIRE(list.contains("pear"));
+        REQUIRE(list.contains("grapefruit"));
+    }
+
+    SECTION("Modify QList") {
+        QList<QString> list = stack.toList();
+        list.removeAll("pear");
+
+        REQUIRE(list.size() == 3);
+        REQUIRE(list.contains("apple") == true);
+        REQUIRE(list.contains("pear") == false);
+
+        REQUIRE(stack.size() == 4);
+        REQUIRE(stack.contains("pear") == true);
+    }
+}
+
+TEST_CASE("QStack swap", "[qstack]")
+{
+   QStack<QString> stack1;
+   stack1.push("watermelon");
+   stack1.push("apple");
+   stack1.push("pear");
+   stack1.push("grapefruit");
+
+   QStack<QString> stack2;
+   stack2.push("grape");
+   stack2.push("orange");
+   stack2.push("peach");
+
+   stack1.swap(stack2);
+
+   REQUIRE(stack1.contains("orange") == true);
+   REQUIRE(stack2.contains("orange") == false);
 }
