663fbee85ff883fa1541dc690f0e429b19a5a6e0
Ansel Sermersheim <ansel@copperspice.com>
2025-05-12 10:24:10 -0700

    Create primary wayland plugin and wayland client library

---
+++ src/plugins/platforms/wayland/client/qwayland_integration.cpp
@@ -0,0 +1,53 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#include <qwayland_integration_p.h>
+
+#include <qapplication_p.h>
+
+namespace QtWaylandClient {
+
+QWaylandIntegration::QWaylandIntegration()
+{
+}
+
+QWaylandIntegration::~QWaylandIntegration()
+{
+}
+
+QPlatformWindow *QWaylandIntegration::createPlatformWindow(QWindow *window) const
+{
+   return nullptr;
+}
+
+QPlatformBackingStore *QWaylandIntegration::createPlatformBackingStore(QWindow *window) const
+{
+   return nullptr;
+}
+
+QAbstractEventDispatcher *QWaylandIntegration::createEventDispatcher() const
+{
+   return nullptr;
+}
+
+}
---
+++ src/plugins/platforms/wayland/client/qwayland_integration_p.h
@@ -0,0 +1,44 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#ifndef QWAYLAND_INTEGRATION_H
+#define QWAYLAND_INTEGRATION_H
+
+#include <qplatform_integration.h>
+
+namespace QtWaylandClient {
+
+class Q_WAYLAND_CLIENT_EXPORT QWaylandIntegration : public QPlatformIntegration
+{
+ public:
+   QWaylandIntegration();
+   ~QWaylandIntegration();
+
+   QPlatformWindow *createPlatformWindow(QWindow *window) const override;
+   QPlatformBackingStore *createPlatformBackingStore(QWindow *window) const override;
+   QAbstractEventDispatcher *createEventDispatcher() const override;
+};
+
+}
+
+#endif
