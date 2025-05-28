e97ef2ea72b706d294e15f7786f35088661f7345
Barbara Geller <barbara@copperspice.com>
2025-05-28 09:22:15 -0700

    Wayland shared files

---
+++ src/plugins/platforms/wayland/shared/qwayland_xkb.h
@@ -0,0 +1,45 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+// ** Copyright (C) 2015 Jolla Ltd
+
+#ifndef QWAYLAND_XKB_H
+#define QWAYLAND_XKB_H
+
+#ifndef QT_NO_WAYLAND_XKB
+
+#include <qnamespace.h>
+#include <qstring.h>
+
+#include <xkbcommon/xkbcommon.h>
+
+class QWaylandXkb
+{
+ public:
+   static int keysymToQtKey(xkb_keysym_t keysym, Qt::KeyboardModifiers &modifiers, const QString &text);
+   static Qt::KeyboardModifiers modifiers(struct xkb_state *state);
+};
+
+#endif // QT_NO_WAYLAND_XKB
+
+#endif
