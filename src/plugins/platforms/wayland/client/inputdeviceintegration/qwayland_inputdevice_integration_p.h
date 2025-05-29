8133bb0bcb5944e4a4e87773d3b5d6095369f41d
Barbara Geller <barbara@copperspice.com>
2025-05-29 11:14:00 -0700

    Input device integration plugin

---
+++ src/plugins/platforms/wayland/client/inputdeviceintegration/qwayland_inputdevice_integration_p.h
@@ -0,0 +1,52 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+// Copyright (C) 2014 LG Electronics Ltd
+
+#ifndef QWAYLAND_INPUTDEVICE_INTEGRATION_H
+#define QWAYLAND_INPUTDEVICE_INTEGRATION_H
+
+#include <qglobal.h>
+
+#include <stdint.h>
+
+namespace QtWaylandClient {
+
+class QWaylandDisplay;
+class QWaylandInputDevice;
+
+class Q_WAYLAND_CLIENT_EXPORT QWaylandInputDeviceIntegration
+{
+ public:
+   QWaylandInputDeviceIntegration()
+   { }
+
+   virtual ~QWaylandInputDeviceIntegration()
+   { }
+
+   virtual QWaylandInputDevice *createInputDevice(QWaylandDisplay *d, int version, uint32_t id) = 0;
+};
+
+}
+
+#endif
