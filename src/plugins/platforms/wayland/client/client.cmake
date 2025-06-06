0489a586d0dc108b4b350840681baf1f84471b76
Barbara Geller <barbara@copperspice.com>
2025-06-06 13:41:17 -0700

    Add hardware buffer integration plugins to CMake build file

--- src/plugins/platforms/wayland/client/client.cmake
+++ src/plugins/platforms/wayland/client/client.cmake
@@ -31,6 +31,14 @@ if(BUILD_PLATFORMS_WAYLAND_PLUGIN)
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/shared/qwayland_shm_formathelper.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/shared/qwayland_xkb.h
 
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integration_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integrationfactory_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integrationplugin_p.h
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integration_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integrationfactory_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integrationplugin_p.h
+
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/inputdeviceintegration/qwayland_inputdevice_integration_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/inputdeviceintegration/qwayland_inputdevice_integrationfactory_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/inputdeviceintegration/qwayland_inputdevice_integrationplugin_p.h
@@ -56,6 +64,14 @@ if(BUILD_PLATFORMS_WAYLAND_PLUGIN)
 
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/shared/qwayland_mimehelper.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/shared/qwayland_xkb.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integration.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integrationfactory.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_clientbuffer_integrationplugin.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integration.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integrationfactory.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/hardwareintegration/qwayland_serverbuffer_integrationplugin.cpp
 
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/inputdeviceintegration/qwayland_inputdevice_integrationfactory.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/inputdeviceintegration/qwayland_inputdevice_integrationplugin.cpp
