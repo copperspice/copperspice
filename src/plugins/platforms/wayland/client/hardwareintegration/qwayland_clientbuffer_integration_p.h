7bf5329b8b5f9d6676aa227785e16933d3d4a93f
Barbara Geller <barbara@copperspice.com>
2025-06-06 13:23:51 -0700

    Hardware buffer integration plugins

---
+++ src/plugins/platforms/wayland/client/hardwareintegration/qwayland_clientbuffer_integration_p.h
@@ -0,0 +1,78 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#ifndef QWAYLAND_CLIENTBUFFER_INTEGRATION_H
+#define QWAYLAND_CLIENTBUFFER_INTEGRATION_H
+
+#include <qglobal.h>
+
+class QPlatformOpenGLContext;
+class QSurfaceFormat;
+class QWindow;
+
+namespace QtWaylandClient {
+
+class QWaylandDisplay;
+class QWaylandWindow;
+
+class Q_WAYLAND_CLIENT_EXPORT QWaylandClientBufferIntegration
+{
+ public:
+   enum NativeResource {
+      EglDisplay,
+      EglConfig,
+      EglContext
+   };
+
+   QWaylandClientBufferIntegration();
+   virtual ~QWaylandClientBufferIntegration();
+
+   virtual void initialize(QWaylandDisplay *display) = 0;
+
+   virtual bool isValid() const {
+      return true;
+   }
+
+   virtual bool supportsThreadedOpenGL() const {
+      return false;
+   }
+
+   virtual bool supportsWindowDecoration() const {
+      return false;
+   }
+
+   virtual QWaylandWindow *createEglWindow(QWindow *window) = 0;
+   virtual QPlatformOpenGLContext *createPlatformOpenGLContext(const QSurfaceFormat &glFormat, QPlatformOpenGLContext *share) const = 0;
+
+   virtual void *nativeResource(NativeResource /*resource*/) {
+      return nullptr;
+   }
+
+   virtual void *nativeResourceForContext(NativeResource /*resource*/, QPlatformOpenGLContext */*context*/) {
+      return nullptr;
+   }
+};
+
+}
+
+#endif
---
+++ src/plugins/platforms/wayland/client/hardwareintegration/qwayland_serverbuffer_integrationfactory.cpp
@@ -0,0 +1,94 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#include <qwayland_serverbuffer_integrationfactory_p.h>
+
+#include <qcoreapplication.h>
+#include <qdir.h>
+
+#include <qfactoryloader_p.h>
+#include <qwayland_serverbuffer_integrationplugin_p.h>
+
+namespace QtWaylandClient {
+
+static QFactoryLoader *loader()
+{
+   static QFactoryLoader retval(QWaylandServerBufferIntegrationFactoryInterface_ID, "/platforms", Qt::CaseInsensitive);
+   return &retval;
+}
+
+static QFactoryLoader *directLoader()
+{
+   static QFactoryLoader retval(QWaylandServerBufferIntegrationFactoryInterface_ID, QString(), Qt::CaseInsensitive);
+   return &retval;
+}
+
+QStringList QWaylandServerBufferIntegrationFactory::keys(const QString &pluginPath)
+{
+   QStringList list;
+
+   if (! pluginPath.isEmpty()) {
+      QCoreApplication::addLibraryPath(pluginPath);
+      list = directLoader()->keySet().toList();
+
+      if (! list.isEmpty()) {
+         const QString postFix = " (from " + QDir::toNativeSeparators(pluginPath) + ')';
+
+         for (auto &item : list) {
+            item.append(postFix);
+         }
+      }
+   }
+
+   list.append(loader()->keySet().toList());
+
+   return list;
+}
+
+QWaylandServerBufferIntegration *QWaylandServerBufferIntegrationFactory::create(const QString &platform,
+      const QStringList &args, const QString &pluginPath)
+{
+   // Try loading the plugin from platformPluginPath first
+
+   if (! pluginPath.isEmpty()) {
+      QCoreApplication::addLibraryPath(pluginPath);
+
+      QWaylandServerBufferIntegration *retval = cs_load_plugin < QWaylandServerBufferIntegration,
+            QWaylandServerBufferIntegrationPlugin > (directLoader(), platform, args);
+
+      if (retval != nullptr) {
+         return retval;
+      }
+   }
+
+   QWaylandServerBufferIntegration *retval = cs_load_plugin < QWaylandServerBufferIntegration,
+         QWaylandServerBufferIntegrationPlugin > (loader(), platform, args);
+
+   if (retval != nullptr) {
+      return retval;
+   }
+
+   return nullptr;
+}
+
+}
