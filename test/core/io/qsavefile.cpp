be8aa9153ff3d848979094b4c49975733790218c
Barbara Geller <barbara@copperspice.com>
2025-08-08 09:41:47 -0700

    Create new unit tests for QSaveFile

--- test/core/io/qsavefile.cpp
+++ test/core/io/qsavefile.cpp
@@ -18,6 +18,7 @@
 ***********************************************************************/
 
 #include <qsavefile.h>
+#include <qtemporarydir.h>
 
 #include <cs_catch2.h>
 
@@ -43,13 +44,92 @@ TEST_CASE("QSaveFile open", "[qsavefile]")
    REQUIRE(file.resize(10) == true);
 }
 
+TEST_CASE("QSaveFile errors", "[qsavefile]")
+{
+   QTemporaryDir tmpDir;
+   REQUIRE(tmpDir.isValid());
+
+   QString fileName = tmpDir.path() + "/error.txt";
+
+   SECTION("a")
+   {
+      QSaveFile file(fileName);
+
+      REQUIRE(file.commit() == false);
+   }
+
+   SECTION("b")
+   {
+      QSaveFile file(fileName);
+
+      REQUIRE(file.open(QIODevice::WriteOnly) == true);
+
+      auto s1 = file.write("ABC");
+      REQUIRE(s1 == 3);
+
+      //
+      REQUIRE(file.commit() == true);
+
+      auto s2 = file.write("ABC");
+      REQUIRE(s2 == -1);
+   }
+}
+
 TEST_CASE("QSaveFile fileName", "[qsavefile]")
 {
    QSaveFile file("c:/machine/path1/dir1/file1");
-
    REQUIRE(file.fileName() == "c:/machine/path1/dir1/file1");
 
    REQUIRE(file.pos() == 0);
    REQUIRE(file.size() == 0);
    REQUIRE(file.resize(10) == false);
+}
+
+TEST_CASE("QSaveFile write", "[qsavefile]")
+{
+   QTemporaryDir tmpDir;
+   REQUIRE(tmpDir.isValid());
+
+   //
+   QByteArray data_in("CopperSpice");
+   QString fileName = tmpDir.path() + "/test.txt";
+
+   QSaveFile file_A(fileName);
+
+   REQUIRE(file_A.open(QIODevice::WriteOnly | QIODevice::Text) == true);
+   REQUIRE(file_A.write(data_in) == data_in.size());
+   REQUIRE(file_A.commit() == true);
+
+   QFile file_B(fileName);
+
+   REQUIRE(file_B.open(QIODevice::ReadOnly | QIODevice::Text) == true);
+
+   QByteArray data_out = file_B.readAll();
+   REQUIRE(data_in == data_out);
+}
+
+TEST_CASE("QSaveFile write_multiple", "[qsavefile]")
+{
+   QTemporaryDir tmpDir;
+   REQUIRE(tmpDir.isValid());
+
+   //
+   QString fileName = tmpDir.path() + "/test.txt";
+
+   QSaveFile file_A(fileName);
+   REQUIRE(file_A.open(QIODevice::WriteOnly));
+
+   REQUIRE(file_A.write("pear\n") > 0);
+   REQUIRE(file_A.write("apple\n") > 0);
+   REQUIRE(file_A.write("watermelon\n") > 0);
+
+   REQUIRE(file_A.commit());
+
+   QFile file_B(fileName);
+   REQUIRE(file_B.open(QIODevice::ReadOnly) == true);
+
+   QByteArray data_in("pear\napple\nwatermelon\n");
+   QByteArray data_out = file_B.readAll();
+
+   REQUIRE(data_in == data_out);
 }
