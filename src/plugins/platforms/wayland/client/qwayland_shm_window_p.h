0825b80a52817169d46ba7d056409dab36259483
Barbara Geller <barbara@copperspice.com>
2025-08-13 13:06:30 -0700

    ShmWindow class

---
+++ src/plugins/platforms/wayland/client/qwayland_shm_window_p.h
@@ -0,0 +1,48 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2025 Barbara Geller
+* Copyright (c) 2012-2025 Ansel Sermersheim
+*
+* Copyright (c) 2015 The Qt Company Ltd.
+* Copyright (c) 2012-2016 Digia Plc and/or its subsidiary(-ies).
+* Copyright (c) 2008-2012 Nokia Corporation and/or its subsidiary(-ies).
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#ifndef QWAYLAND_SHM_WINDOW_H
+#define QWAYLAND_SHM_WINDOW_H
+
+#include <qregion.h>
+
+#include <qwayland_window_p.h>
+
+namespace QtWaylandClient {
+
+class Q_WAYLAND_CLIENT_EXPORT QWaylandShmWindow : public QWaylandWindow
+{
+ public:
+   QWaylandShmWindow(QWindow *window);
+   ~QWaylandShmWindow();
+
+   QSurfaceFormat format() const override {
+      return QSurfaceFormat();
+   }
+
+   WindowType windowType() const override;
+};
+
+}
+
+#endif
