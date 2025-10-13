87c7a5d480b22e22d06a2a70d4c116a1c21f44ec
Barbara Geller <barbara@copperspice.com>
2025-10-13 11:26:44 -0700

    Add additional unit tests for QPointF

--- test/core/tools/qpointf.cpp
+++ test/core/tools/qpointf.cpp
@@ -32,21 +32,90 @@ TEST_CASE("QPointF traits", "[qpointf]")
    REQUIRE(std::has_virtual_destructor_v<QPointF> == false);
 }
 
+TEST_CASE("QPointF comparison", "[qpointf]")
+{
+   QPointF data_a(5, 5);
+   QPointF data_b(5, 5);
+
+   REQUIRE(data_a == data_b);
+   REQUIRE((data_a != data_b) == false);
+
+   //
+   data_b = QPointF(5, 20);
+
+   REQUIRE(data_a != data_b);
+   REQUIRE((data_a == data_b) == false);
+}
+
 TEST_CASE("QPointF constructor", "[qpointf]")
 {
    QPointF data(75, 125);
 
-   REQUIRE(! data.isNull());
+   REQUIRE(data.isNull() == false);
 
    REQUIRE(data.x() == 75);
    REQUIRE(data.y() == 125);
+
+   //
+   data = QPointF(5, -50);
+
+   REQUIRE(data.isNull() == false);
+
+   REQUIRE(data.x() ==  5);
+   REQUIRE(data.y() == -50);
 }
 
-TEST_CASE("QPointF empty", "[qpointf]")
+TEST_CASE("QPointF copy_assign", "[qpointf]")
+{
+   QPointF data_a(43, 72);
+   QPointF data_b(data_a);
+
+   REQUIRE(data_b.x() == 43);
+   REQUIRE(data_b.y() == 72);
+
+   //
+   data_b = data_a;
+
+   REQUIRE(data_a == data_b);
+   REQUIRE(data_b == data_a);
+}
+
+TEST_CASE("QPointF dotProduct", "[qpointf]")
+{
+   QPointF data_a(3, 4);
+   QPointF data_b(5, 6);
+
+   REQUIRE(QPointF::dotProduct(data_a, data_b) == (3*5 + 4*6));
+
+   //
+   data_a = QPointF(8.3,  7.9);
+   data_b = QPointF(-1.2, 12.5);
+
+   qreal result_1 = QPointF::dotProduct(data_a, data_b);
+   qreal result_2 = (8.3 * (-1.2) + 7.9 * 12.5);
+
+   // 88.79
+   REQUIRE_THAT(result_1, Catch::Matchers::WithinAbs(result_2, 0.01));
+}
+
+TEST_CASE("QPointF isNull", "[qpoint")
 {
    QPointF data;
 
-   REQUIRE(data.isNull());
+   REQUIRE(data.isNull() == true);
+
+   REQUIRE(data.x() == 0);
+   REQUIRE(data.y() == 0);
+
+   //
+   data = QPointF(0, 0);
+
+   REQUIRE(data.isNull() == true);
+
+   //
+   data = QPointF(10, 0);
+
+   REQUIRE(data.isNull() == false);
 }
 
 TEST_CASE("QPointF manhattanLength", "[qpointf]")
@@ -92,21 +161,33 @@ TEST_CASE("QPointF manhattanLength", "[qpointf]")
 
 TEST_CASE("QPointF math", "[qpointf]")
 {
-   QPointF data1(50, 125);
-   QPointF data2(10, 20);
+   QPointF data_a(50, 125);
+   QPointF data_b(10, 20);
 
    SECTION("plus") {
-      data1 += data2;
+      data_a += data_b;
 
-      REQUIRE(data1.x()  == 60);
-      REQUIRE(data1.y() == 145);
+      REQUIRE(data_a.x() == 60);
+      REQUIRE(data_a.y() == 145);
+
+      //
+      QPointF result = data_a + data_b;
+
+      REQUIRE(result.x() == 70);
+      REQUIRE(result.y() == 165);
    }
 
    SECTION("minus") {
-      data1 -= data2;
+      data_a -= data_b;
 
-      REQUIRE(data1.x()  == 40);
-      REQUIRE(data1.y() == 105);
+      REQUIRE(data_a.x() == 40);
+      REQUIRE(data_a.y() == 105);
+
+      //
+      QPointF result = data_a - data_b;
+
+      REQUIRE(result.x() == 30);
+      REQUIRE(result.y() == 85);
    }
 }
 
@@ -127,12 +208,16 @@ TEST_CASE("QPointF multiply", "[qpointf]")
 
    {
       data   = QPointF(5, 10);
-      factor = 0.50;
+      factor = 0.501;
 
       data *= factor;
 
-      REQUIRE(data.x() == 2.5);
-      REQUIRE(data.y() == 5.0);
+      REQUIRE(data.x() == 2.505);
+      REQUIRE(data.y() == 5.010);
+   }
+
+   {
+      REQUIRE(data * 4 == QPointF(10.02, 20.04));
    }
 }
 
@@ -160,6 +245,10 @@ TEST_CASE("QPointF divide", "[qpointf]")
       REQUIRE(data.x() == -2.5);
       REQUIRE(data.y() == -5);
    }
+
+   {
+      REQUIRE(data / 2 == QPointF(-1.25, -2.5));
+   }
 }
 
 TEST_CASE("QPointF rx_ry", "[qpoint")
@@ -168,20 +257,20 @@ TEST_CASE("QPointF rx_ry", "[qpoint")
 
    {
       QPointF data2(data1);
-
       ++data2.rx();
-      REQUIRE(data2.x() == data1.x() + 1);
+
+      REQUIRE(data2.x() == 0);
    }
 
    {
       QPointF data2(data1);
-
       ++data2.ry();
-      REQUIRE(data2.y() == data1.y() + 1);
+
+      REQUIRE(data2.y() == 1);
    }
 }
 
-TEST_CASE("QPointF set_get", "[qpointf]")
+TEST_CASE("QPointF set", "[qpoint")
 {
    QPointF data;
 
@@ -213,4 +302,3 @@ TEST_CASE("QPointF set_get", "[qpointf]")
       REQUIRE(data.y() ==  maxValue);
    }
 }
-
