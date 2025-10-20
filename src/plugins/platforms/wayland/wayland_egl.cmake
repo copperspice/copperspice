330298f892ede5f9c8b16e62a51951541608acb3
Barbara Geller <barbara@copperspice.com>
2025-10-20 11:24:03 -0700

    New CMake build files for Wayland EGL plugin

---
+++ src/plugins/platforms/wayland/wayland_egl.cmake
@@ -0,0 +1,44 @@
+if(BUILD_PLATFORMS_WAYLAND_PLUGIN)
+   add_library(CsGuiWayland_Egl MODULE "")
+   add_library(CopperSpice::CsGuiWayland_Egl ALIAS CsGuiWayland_Egl)
+
+   set_target_properties(CsGuiWayland_Egl PROPERTIES
+      OUTPUT_NAME CsGuiWayland_Egl${BUILD_ABI} PREFIX ""
+      INSTALL_RPATH "$ORIGIN/.."
+   )
+
+   list(APPEND PLATFORMS_WAYLAND_EGL_PRIVATE_INCLUDES
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/egl/qwayland_egl_clientbuffer_integration.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/egl/qwayland_egl_forward.h
+   )
+
+   target_sources(CsGuiWayland_Egl
+      PRIVATE
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/main_egl.cpp
+
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/egl/qwayland_egl_clientbuffer_integration.cpp
+   )
+
+   macro_generate_misc_private("${PLATFORMS_WAYLAND_EGL_PRIVATE_INCLUDES}" QtGui/private/platforms)
+
+   target_sources(CsGuiWayland_Egl
+      PRIVATE
+      ${PLATFORMS_WAYLAND_EGL_PRIVATE_INCLUDES}
+   )
+
+   target_link_libraries(CsGuiWayland_Egl
+      PRIVATE
+      CsCore
+      CsGui
+      CsWaylandClient
+      Wayland::EGL
+      OpenGL::EGL
+   )
+
+   target_compile_definitions(CsGuiWayland_Egl
+      PRIVATE
+      -DQT_PLUGIN
+   )
+
+   install(TARGETS CsGuiWayland_Egl DESTINATION ${CMAKE_INSTALL_LIBDIR})
+endif()
