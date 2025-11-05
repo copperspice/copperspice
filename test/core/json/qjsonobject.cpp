db9fcf33cff08354f49198956588dd62447191c2
Barbara Geller <barbara@copperspice.com>
2025-11-05 12:17:50 -0800

    Include copy and move unit tests for QJsonObject

--- test/core/json/qjsonobject.cpp
+++ test/core/json/qjsonobject.cpp
@@ -18,7 +18,10 @@
 ***********************************************************************/
 
 #include <qstring8.h>
+
+#include <qjsonarray.h>
 #include <qjsonobject.h>
+#include <qjsonvalue.h>
 
 #include <catch2/catch.hpp>
 
@@ -33,3 +36,62 @@ TEST_CASE("QJsonObject traits", "[qjsonobject]")
    REQUIRE(std::has_virtual_destructor_v<QJsonObject> == false);
 }
 
+TEST_CASE("QJsonObject copy_assign", "[qjsonobject]")
+{
+   QJsonObject data_a;
+
+   QJsonValue value = QJsonValue(QString("MM/dd/yyyy"));
+   data_a.insert("formatDate", value);
+
+   QJsonArray list;
+   list.append(QString("d:/cs_docs/file1.dox"));
+   data_a.insert("file-names", list);
+
+   //
+   QJsonObject data_b(data_a);
+
+   REQUIRE(data_a.count() == 2);
+   REQUIRE(data_b.count() == 2);
+
+   REQUIRE(data_a.value("formatDate") == QString("MM/dd/yyyy"));
+   REQUIRE(data_b.value("formatDate") == QString("MM/dd/yyyy"));
+
+   //
+   QJsonValue data_c;
+   data_c = data_a;
+
+   REQUIRE(data_a.count() == 2);
+   REQUIRE(data_b.count() == 2);
+
+   REQUIRE(data_a.value("file-names") == list);
+   REQUIRE(data_b.value("file-names") == list);
+}
+
+TEST_CASE("QJsonObject move_assign", "[qjsonobject]")
+{
+   QJsonObject data_a;
+
+   QJsonValue value = QJsonValue(QString("MM/dd/yyyy"));
+   data_a.insert("formatDate", value);
+
+   QJsonArray list;
+   list.append(QString("d:/cs_docs/file1.dox"));
+   data_a.insert("file-names", list);
+
+   QJsonObject data_b(std::move(data_a));
+
+   //
+   REQUIRE(data_b.count() == 2);
+
+   REQUIRE(data_b.value("formatDate") == QString("MM/dd/yyyy"));
+   REQUIRE(data_b.value("file-names") == list);
+
+   //
+   QJsonObject data_c;
+   data_c = std::move(data_b);
+
+   REQUIRE(data_c.count() == 2);
+
+   REQUIRE(data_c.value("formatDate") == QString("MM/dd/yyyy"));
+   REQUIRE(data_c.value("file-names") == list);
+}
