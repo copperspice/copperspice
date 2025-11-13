582b94230c03378dce6403958f12fd15b5b603c2
Barbara Geller <barbara@copperspice.com>
2025-11-12 17:20:31 -0800

    Add QWaylandXdgPopup, context menu or tooltip

--- src/plugins/platforms/wayland/client/client.cmake
+++ src/plugins/platforms/wayland/client/client.cmake
@@ -43,6 +43,7 @@ if(BUILD_PLATFORMS_WAYLAND_PLUGIN)
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_touch_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_window_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_windowmanager_integration_p.h
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_popup_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_shell_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_shellintegration_p.h
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_surface_p.h
@@ -104,6 +105,7 @@ if(BUILD_PLATFORMS_WAYLAND_PLUGIN)
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_touch.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_window.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_windowmanager_integration.cpp
+      ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_popup.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_shell.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_shellintegration.cpp
       ${CMAKE_CURRENT_SOURCE_DIR}/wayland/client/qwayland_xdg_surface.cpp
