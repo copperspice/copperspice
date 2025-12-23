87cbf00d7a2ba3d9233773a007f658f6c572a88d
Barbara Geller <barbara@copperspice.com>
2025-12-23 10:44:24 -0800

    Change QMetaEnum valueToKeys() to support alias values

--- src/core/kernel/csmeta_enum.cpp
+++ src/core/kernel/csmeta_enum.cpp
@@ -26,6 +26,7 @@
 #include <csmeta.h>
 
 #include <qmetaobject.h>
+#include <qset.h>
 #include <qstringlist.h>
 #include <qstringparser.h>
 
@@ -153,15 +154,27 @@ QString QMetaEnum::valueToKeys(int value) const
 {
    QString keys;
 
+   // has this enum value been used
+   QSet<int> valueUsed;
+
    for (auto elem = m_data.begin(); elem != m_data.end(); ++elem) {
 
-      if (elem.value() & value) {
+      auto elementValue = elem.value();
+      const auto &elementKey = elem.key();
+
+      if ((elementValue & value) == elementValue) {
+
+         if (valueUsed.contains(elementValue )) {
+            continue;
+         }
+
+         valueUsed.insert(elementValue);
 
          if (keys.isEmpty()) {
-            keys = elem.key();
+            keys = elementKey;
 
          } else  {
-            keys = keys + '|' + elem.key();
+            keys = keys + '|' + elementKey;
 
          }
       }
