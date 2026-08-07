4d733c6e51e5eb933e978170821aa6df64af3e62
Old-Rockin-Chair-Joe <167979918+Old-Rockin-Chair-Joe@users.noreply.github.com>
2026-08-07 23:21:32 +0700

    Update qsimd.cpp

--- src/core/tools/qsimd.cpp
+++ src/core/tools/qsimd.cpp
@@ -217,14 +217,12 @@ static void cpuidFeatures07_00(uint &ebx, uint &ecx)
 #endif
 }
 
-#ifdef Q_OS_WIN
-#ifndef Q_CC_CLANG
+#if defined(Q_OS_WIN) && !(defined(Q_CC_MINGW) && defined(Q_CC_CLANG))
    // fallback overload in case this intrinsic does not exist: unsigned __int64 _xgetbv(unsigned int);
    inline quint64 _xgetbv(__int64)
    {
       return 0;
    }
-#endif
 #endif
 
 static void xgetbv(uint in, uint &eax, uint &edx)
